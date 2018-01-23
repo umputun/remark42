446b80b5c9d59b68acbdebb5455bd24c47274fd1
igoradamenko <mail@igoradamenko.com>
2018-01-23 12:22:29 +0200

    prepare webpack config to preact

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,35 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const HtmlPlugin = require('html-webpack-plugin');
+
 const webpack = require('webpack');
+const ExtractText = require('extract-text-webpack-plugin');
+const Clean = require('clean-webpack-plugin');
+const Html = require('html-webpack-plugin');
+const Provide = webpack.ProvidePlugin;
+const Define = webpack.DefinePlugin;
 
 const publicFolder = path.resolve(__dirname, 'public');
 const env = process.env.NODE_ENV || 'dev';
 const hash = env === 'production' ? '.[chunkhash]' : '.[hash]';
-
-const extractCSS = new ExtractTextPlugin({
-  filename: `app${hash}.css`,
-  allChunks: true
-});
-const cleanPublic = new CleanPlugin(publicFolder);
-const uglifyJS = new webpack.optimize.UglifyJsPlugin();
-const ModuleConcatenation = new webpack.optimize.ModuleConcatenationPlugin();
-const Manifest = new ManifestPlugin();
-const Html = new HtmlPlugin({ template: path.resolve(__dirname, 'index.ejs') })
-
-const postcssLoader = {
-  loader: 'postcss-loader',
-  options: {
-    plugins: [
-      require('autoprefixer')({ browsers: ['> 1%'] }),
-      require('postcss-url')({ url: 'inline', maxSize: 5 }),
-      require('postcss-csso')
-    ]
-  }
-};
 
 module.exports = {
   context: __dirname,
@@ -42,12 +23,13 @@ module.exports = {
     filename: `app${hash}.js`
   },
   resolve: {
-    modules: [ path.resolve(__dirname), 'node_modules' ]
+    extensions: ['.jsx', '.js'],
+    modules: [path.resolve(__dirname, 'app'), path.resolve(__dirname, 'node_modules')]
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /(node_modules|\.vendor\.js$)/,
         use: {
           loader: 'babel-loader',
@@ -58,21 +40,26 @@ module.exports = {
                 useBuiltIns: true,
               }],
             ],
-            plugins: ['transform-object-rest-spread'],
+            plugins: ['transform-object-rest-spread', ['transform-react-jsx', { 'pragma': 'h' }]],
           }
         }
       },
       {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader', postcssLoader ]
-        // TODO: style-loader must be turned on only for dev; for build we need extractCSS
-      },
-      {
         test: /\.scss$/,
         use: [
+          // TODO: style-loader must be turned on only for dev; for build we need extractCSS
           'style-loader',
           'css-loader',
-          postcssLoader,
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('autoprefixer')({browsers: ['> 1%']}),
+                require('postcss-url')({url: 'inline', maxSize: 5}),
+                require('postcss-csso')
+              ]
+            }
+          },
           {
             loader: 'sass-loader',
             options: {
@@ -93,12 +80,20 @@ module.exports = {
     ]
   },
   plugins: [
-    cleanPublic,
-    Html, // we should add it only in dev
-    extractCSS,
-    // uglifyJS,
-    ModuleConcatenation,
-    Manifest,
+    new Clean(publicFolder),
+    new Provide({
+      b: 'bem-react-helper',
+    }),
+    new Define({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    new Html({ template: path.resolve(__dirname, 'index.ejs') }), // we should add it only in dev
+    new ExtractText({
+      filename: `app${hash}.css`,
+      allChunks: true
+    }),
+    // new webpack.optimize.UglifyJsPlugin(), // TODO: enable it
+    new webpack.optimize.ModuleConcatenationPlugin(),
   ],
   watch: env === 'dev',
   watchOptions: {
