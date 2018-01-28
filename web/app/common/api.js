73861df6b104a9d9e75e306c8a60d792dd447279
igoradamenko <mail@igoradamenko.com>
2018-01-29 00:39:14 +0200

    add send api endpoint for posting comments

--- web/app/common/api.js
+++ web/app/common/api.js
@@ -1,10 +1,15 @@
+import { siteId, url } from './settings';
+
 import fetcher from './fetcher'
 
 export const find = ({ url }) => fetcher.get(`/find?url=${url}&sort=time&format=tree`);
 
-export const vote = ({ id, url, value }) => fetcher.get(`/vote/${id}?url=${url}&vote=${value}`);
+export const send = ({ text }) => fetcher.post('/comment', { text, locator: { site: siteId, url } });
+
+export const vote = ({ id, url, value }) => fetcher.put(`/vote/${id}?url=${url}&vote=${value}`);
 
 export default {
   find,
+  send,
   vote,
 };
