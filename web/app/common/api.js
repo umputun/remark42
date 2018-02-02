45dc4a49e51871ed662e01de973b7521137dfaa9
igoradamenko <mail@igoradamenko.com>
2018-02-02 20:52:44 +0200

    add user get request to api

--- web/app/common/api.js
+++ web/app/common/api.js
@@ -4,6 +4,8 @@ import fetcher from './fetcher'
 
 export const find = ({ url }) => fetcher.get(`/find?url=${url}&sort=time&format=tree`);
 
+export const user = () => fetcher.get('/user');
+
 export const send = ({ text }) => fetcher.post('/comment', { text, locator: { site: siteId, url } });
 
 export const vote = ({ id, url, value }) => fetcher.put(`/vote/${id}?url=${url}&vote=${value}`);
@@ -12,4 +14,5 @@ export default {
   find,
   send,
   vote,
+  user,
 };
