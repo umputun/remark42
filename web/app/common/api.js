46c52c6e46b891b042c0e73e037c7aee57115411
igoradamenko <mail@igoradamenko.com>
2018-02-03 01:01:38 +0200

    change default sort to -score

--- web/app/common/api.js
+++ web/app/common/api.js
@@ -2,7 +2,7 @@ import { siteId, url } from './settings';
 
 import fetcher from './fetcher'
 
-export const find = ({ url }) => fetcher.get(`/find?url=${url}&sort=time&format=tree`);
+export const find = ({ url }) => fetcher.get(`/find?url=${url}&sort=-score&format=tree`);
 
 export const getComment = ({ id }) => fetcher.get(`/id/${id}?url=${url}`);
 
