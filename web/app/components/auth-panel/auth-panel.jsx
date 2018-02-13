8e62f1b3aae6118d66506e6831a6ad1435aeecd6
igoradamenko <mail@igoradamenko.com>
2018-02-14 01:15:11 +0200

    add auth-panel block

---
+++ web/app/components/auth-panel/auth-panel.jsx
@@ -0,0 +1,42 @@
+import { h, Component } from 'preact';
+
+export default class AuthPanel extends Component {
+  render(props, state) {
+    const { user, providers } = props;
+
+    return (
+      <div className={b('auth-panel', props)}>
+        {
+          !!user.name && (
+            <span>You signed in as <strong>{user.name}</strong>. <span className="auth-panel__pseudo-link" onClick={props.onSignOut}>Sign out?</span></span>
+          )
+        }
+
+        {
+          !user.name && (
+            <span>
+              Sign in to comment using
+              {' '}
+              {
+                providers.map((provider, i) => {
+                  const comma = i === 0 ? '' : ', ';
+
+                  return (
+                    <span>
+                      {comma}
+                      <span
+                        className="auth-panel__pseudo-link"
+                        onClick={() => props.onSignIn(provider)}
+                      >{provider}</span>
+                    </span>
+                  )
+                })
+              }
+              {'.'}
+            </span>
+          )
+        }
+      </div>
+    );
+  }
+}
---
+++ web/app/components/auth-panel/index.js
@@ -0,0 +1,5 @@
+export { default } from './auth-panel';
+
+require('./auth-panel.scss');
+
+require('./__pseudo-link/auth-panel__pseudo-link.scss');
