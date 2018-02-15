1e4704d01eca2afb8597256c1d095132f72de181
igoradamenko <mail@igoradamenko.com>
2018-02-15 23:33:13 +0200

    fix missing function wrapper around init js

--- web/app/remark.js
+++ web/app/remark.js
@@ -6,11 +6,15 @@ import Root from './components/root';
 
 import { id } from './common/settings';
 
-const node = document.getElementById(id);
+init();
 
-if (!node) {
-  console.error('Remark42: Can\'t find root node.');
-  return;
+function init() {
+  const node = document.getElementById(id);
+
+  if (!node) {
+    console.error('Remark42: Can\'t find root node.');
+    return;
+  }
+
+  render(<Root/>, node.parentElement, node);
 }
-
-render(<Root/>, node.parentElement, node);
