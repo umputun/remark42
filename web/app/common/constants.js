68fdba30a9ff571f6a380aa02158b5e6b1ced79d
igoradamenko <mail@igoradamenko.com>
2018-03-11 20:33:34 +0300

    add constants for last comments (for node classname & default value of max)

--- web/app/common/constants.js
+++ web/app/common/constants.js
@@ -2,10 +2,14 @@ const BASE_URL = 'https://demo.remark42.com';
 const API_BASE = '/api/v1';
 const NODE_ID = 'remark42';
 const COUNTER_NODE_CLASSNAME = 'remark42__counter';
+const LAST_COMMENTS_NODE_CLASSNAME = 'remark42__last-comments';
+const LAST_COMMENTS_DEFAULT_MAX = 15;
 
 module.exports = {
   BASE_URL,
   API_BASE,
   NODE_ID,
   COUNTER_NODE_CLASSNAME,
+  LAST_COMMENTS_NODE_CLASSNAME,
+  LAST_COMMENTS_DEFAULT_MAX,
 };
