0c90f2df28ca00b2e25ea7d306054106516bcad4
Umputun <umputun@gmail.com>
2018-04-14 14:42:59 -0500

    mkdir for avatars directory

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ sed -i "s|BASE_URL:\"[^\"]*\"|BASE_URL:\"${REMARK_URL}\"|g" /srv/web/*.js
 sed -i "s|var baseurl = '[^']*';|var baseurl = '${REMARK_URL}';|g" /srv/web/*.html
 
 # copy default avatar
+mkdir -p /srv/var/avatars
 cp -fv /srv/web/remark.image /srv/var/avatars/remark.image
 
 echo "start remark42 server"
