5778c0385d16ecac1c297c934b07031d865352e5
igoradamenko <mail@igoradamenko.com>
2018-05-06 23:21:03 +0300

    remove outline from sort select

--- web/app/components/auth-panel/index.js
+++ web/app/components/auth-panel/index.js
@@ -4,6 +4,7 @@ require('./auth-panel.scss');
 
 require('./__column/auth-panel__column.scss');
 require('./__pseudo-link/auth-panel__pseudo-link.scss');
+require('./__select/auth-panel__select.scss');
 require('./__sort/auth-panel__sort.scss');
 require('./__username/auth-panel__username.scss');
 require('./__user-id/auth-panel__user-id.scss');
