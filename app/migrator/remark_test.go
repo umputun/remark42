9f69ffe51cacedd0c4fb6d4466a00b642aea75ee
Kirill Zhuravlev <kazhuravlev@fastmail.com>
2018-05-19 18:17:32 +0300

    change location in prep data for repeatable tests

--- app/migrator/remark_test.go
+++ app/migrator/remark_test.go
@@ -28,7 +28,7 @@ func TestRemark_Export(t *testing.T) {
 	c1, err := buf.ReadString('\n')
 	assert.Nil(t, err)
 	log.Print(c1)
-	exp := `{"id":"efbc17f177ee1a1c0ee6e1e025749966ec071adc","pid":"","text":"some text, <a href=\"http://radio-t.com\" rel=\"nofollow\">link</a>","user":{"name":"user name","id":"user1","picture":"","admin":false},"locator":{"site":"radio-t","url":"https://radio-t.com"},"score":0,"votes":{},"time":"2017-12-20T15:18:22-06:00"}` + "\n"
+	exp := `{"id":"efbc17f177ee1a1c0ee6e1e025749966ec071adc","pid":"","text":"some text, <a href=\"http://radio-t.com\" rel=\"nofollow\">link</a>","user":{"name":"user name","id":"user1","picture":"","admin":false},"locator":{"site":"radio-t","url":"https://radio-t.com"},"score":0,"votes":{},"time":"2017-12-20T15:18:22Z"}` + "\n"
 	assert.Equal(t, exp, c1)
 }
 
@@ -69,7 +69,7 @@ func prep(t *testing.T) *service.DataStore {
 	comment := store.Comment{
 		ID:        "efbc17f177ee1a1c0ee6e1e025749966ec071adc",
 		Text:      `some text, <a href="http://radio-t.com">link</a>`,
-		Timestamp: time.Date(2017, 12, 20, 15, 18, 22, 0, time.Local),
+		Timestamp: time.Date(2017, 12, 20, 15, 18, 22, 0, time.UTC),
 		Locator:   store.Locator{URL: "https://radio-t.com", SiteID: "radio-t"},
 		User:      store.User{ID: "user1", Name: "user name"},
 	}
@@ -77,7 +77,7 @@ func prep(t *testing.T) *service.DataStore {
 	assert.Nil(t, err)
 
 	comment = store.Comment{
-		Text: "some text2", Timestamp: time.Date(2017, 12, 20, 15, 18, 23, 0, time.Local),
+		Text: "some text2", Timestamp: time.Date(2017, 12, 20, 15, 18, 23, 0, time.UTC),
 		Locator: store.Locator{URL: "https://radio-t.com", SiteID: "radio-t"},
 		User:    store.User{ID: "user1", Name: "user name"},
 	}
