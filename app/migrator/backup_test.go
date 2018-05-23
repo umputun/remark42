65d0f799e7bc45b84de1dc2e52f59dbb23da1fe9
Umputun <umputun@gmail.com>
2018-05-23 00:46:13 -0500

    add test for autobackup Do

--- app/migrator/backup_test.go
+++ app/migrator/backup_test.go
@@ -1,6 +1,7 @@
 package migrator
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -47,6 +48,26 @@ func TestMigrator_MakeBackup(t *testing.T) {
 	expFile := fmt.Sprintf("/tmp/remark-backups.test/backup-site1-%s.gz", time.Now().Format("20060102"))
 	assert.Equal(t, expFile, fname)
 
+	fi, err := os.Lstat(expFile)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(52), fi.Size())
+}
+
+func TestMigrator_Do(t *testing.T) {
+	loc := "/tmp/remark-backups.test"
+	defer os.RemoveAll(loc)
+	os.MkdirAll(loc, 0700)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		time.Sleep(time.Second)
+		cancel()
+	}()
+
+	bk := AutoBackup{BackupLocation: loc, SiteID: "site1", KeepMax: 3, Exporter: &mockExporter{}, Duration: 600 * time.Millisecond}
+	bk.Do(ctx)
+
+	expFile := fmt.Sprintf("/tmp/remark-backups.test/backup-site1-%s.gz", time.Now().Format("20060102"))
 	fi, err := os.Lstat(expFile)
 	assert.NoError(t, err)
 	assert.Equal(t, int64(52), fi.Size())
