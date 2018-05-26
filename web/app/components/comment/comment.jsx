4a39daa5521f7875dfbf56cbc6bb3f6b9da7ec97
igoradamenko <mail@igoradamenko.com>
2018-05-26 18:41:21 +0300

    fix condition for comment.orig in comment component

--- web/app/components/comment/comment.jsx
+++ web/app/components/comment/comment.jsx
@@ -286,12 +286,16 @@ export default class Comment extends Component {
               )
           ),
       time: formatTime(new Date(data.time)),
-      orig: isEditing && data.orig && data.orig
-        .replace(/&[#A-Za-z0-9]+;/gi, entity => {
-          const span = document.createElement('span');
-          span.innerHTML = entity;
-          return span.innerText;
-        }),
+      orig: isEditing
+        ? (
+          data.orig && data.orig
+            .replace(/&[#A-Za-z0-9]+;/gi, entity => {
+              const span = document.createElement('span');
+              span.innerHTML = entity;
+              return span.innerText;
+            })
+        )
+        : data.orig,
       score: {
         value: Math.abs(score),
         sign: score > 0 ? '+' : (score < 0 ? '−' : null),
