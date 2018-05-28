732fb65d1b2bf90a1833c35a1f0dbc3d8cdbc843
Umputun <umputun@gmail.com>
2018-05-28 12:47:58 -0500

    default backup path

--- scripts/create-backup.sh
+++ scripts/create-backup.sh
@@ -2,6 +2,7 @@
 
 # this scrips makes a backup file to /srv/var/userbackup-<site>-<timestamp>.gz
 
+BACKUP_PATH=${BACKUP_PATH:-./var}
 backup_file=${BACKUP_PATH}/userbackup-${1}-$(date +%s).gz
 echo "make backup file for site $1 to $backup_file"
 curl "http://127.0.0.1:8081/api/v1/admin/export?site=${1}&secret=${SECRET}" > ${backup_file}
