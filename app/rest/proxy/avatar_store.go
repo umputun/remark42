8cd2627888a5808bf1c843223d35bf2d33ca7b46
Anatoly Milkov <anatoly.milko@gmail.com>
2018-06-16 02:47:30 -0500

    add avatar resizing #78

--- app/rest/proxy/avatar_store.go
+++ app/rest/proxy/avatar_store.go
@@ -1,16 +1,25 @@
 package proxy
 
 import (
+	"bytes"
 	"fmt"
 	"hash/crc64"
+	"image"
+	"image/png"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"path"
 	"strings"
 	"sync"
 
+	// Initializing packages for supporting GIF and JPEG formats.
+	_ "image/gif"
+	_ "image/jpeg"
+
 	"github.com/pkg/errors"
+	"golang.org/x/image/draw"
 
 	"github.com/umputun/remark/app/store"
 )
@@ -23,19 +32,19 @@ type AvatarStore interface {
 
 // FSAvatarStore implements AvatarStore for local file system
 type FSAvatarStore struct {
-	storePath string
-	ctcTable  *crc64.Table
-	once      sync.Once
+	storePath   string
+	resizeLimit int
+	ctcTable    *crc64.Table
+	once        sync.Once
 }
 
 // NewFSAvatarStore makes file-system avatar store
-func NewFSAvatarStore(storePath string) *FSAvatarStore {
-	return &FSAvatarStore{storePath: storePath}
+func NewFSAvatarStore(storePath string, resizeLimit int) *FSAvatarStore {
+	return &FSAvatarStore{storePath: storePath, resizeLimit: resizeLimit}
 }
 
 // Put avatar for userID to file and return avatar's file name (base), like 12345678.image
 func (fs *FSAvatarStore) Put(userID string, reader io.Reader) (avatar string, err error) {
-
 	id := store.EncodeID(userID)
 	location := fs.location(id) // location adds partition to path
 
@@ -56,7 +65,22 @@ func (fs *FSAvatarStore) Put(userID string, reader io.Reader) (avatar string, er
 		}
 	}()
 
-	if _, err = io.Copy(fh, reader); err != nil {
+	img, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return "", errors.Wrapf(err, "can't read original avatar content %s", avFile)
+	}
+	// Trying to resize avatar, using original, if failing.
+	if fs.resizeLimit > 0 {
+		resized, e := resize(img, fs.resizeLimit)
+		if e != nil {
+			log.Printf("[WARN] eor on resize avatar for user %s, %s", userID, e)
+		}
+		if resized != nil {
+			img = resized
+		}
+	}
+
+	if _, err = fh.Write(img); err != nil {
 		return "", errors.Wrapf(err, "can't save file %s", avFile)
 	}
 	return id + imgSfx, nil
@@ -84,4 +108,46 @@ func (fs *FSAvatarStore) location(id string) string {
 	checksum64 := crc64.Checksum([]byte(id), fs.ctcTable)
 	partition := checksum64 % 100
 	return path.Join(fs.storePath, fmt.Sprintf("%02d", partition))
+}
+
+// Resizes an image of supported format (PNG, JPG, GIF) to the size of "limit" px of the biggest side
+// (width or height) preserving aspect ratio.
+// Reruns nil if resizing is not needed.
+func resize(img []byte, limit int) ([]byte, error) {
+	if limit <= 0 {
+		return nil, errors.New("limit should be greater than 0")
+	}
+	src, _, err := image.Decode(bytes.NewReader(img))
+	if err != nil {
+		return nil, errors.Wrap(err, "can't decode avatar image")
+	}
+
+	bounds := src.Bounds()
+	w, h := bounds.Dx(), bounds.Dy()
+	if w <= limit && h <= limit || w <= 0 || h <= 0 {
+		// If resizing image is smaller that the limit or has 0 size, return reader unchenged.
+		return nil, nil
+	}
+	var newW, newH int
+	if w > h {
+		newW, newH = limit, h*limit/w
+	} else {
+		newW, newH = w*limit/h, limit
+	}
+	m := image.NewRGBA(image.Rect(0, 0, newW, newH))
+	// draw.ApproxBiLinear.Scale(m, m.Bounds(), src, src.Bounds(), draw.Src, nil)
+	draw.BiLinear.Scale(m, m.Bounds(), src, src.Bounds(), draw.Src, nil) // Slower but better quality.
+
+	var out bytes.Buffer
+	if err = png.Encode(&out, m); err != nil {
+		return nil, errors.Wrapf(err, "can't encode resized avatar buffer %s, id")
+	}
+
+	// write new image to file
+	outBytes, err := ioutil.ReadAll(&out)
+	if err != nil {
+		return nil, errors.Wrap(err, "can't read resized avatar buffer")
+	}
+
+	return outBytes, nil
 }
--- app/rest/proxy/avatar_store_test.go
+++ app/rest/proxy/avatar_store_test.go
@@ -1,6 +1,8 @@
 package proxy
 
 import (
+	"bytes"
+	"image"
 	"io/ioutil"
 	"os"
 	"strings"
@@ -11,7 +13,7 @@ import (
 )
 
 func TestAvatarStore_Put(t *testing.T) {
-	p := NewFSAvatarStore("/tmp/avatars.test")
+	p := NewFSAvatarStore("/tmp/avatars.test", 300)
 	os.MkdirAll("/tmp/avatars.test", 0700)
 	defer os.RemoveAll("/tmp/avatars.test")
 
@@ -29,13 +31,23 @@ func TestAvatarStore_Put(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, int64(25), fi.Size())
 
-	p = NewFSAvatarStore("/dev/null")
+	// with resize
+	file, e := os.Open("testdata/circles.png")
+	require.Nil(t, e)
+	avatar, err = p.Put("user3", file)
+	require.Nil(t, err)
+	assert.Equal(t, "0b7f849446d3383546d15a480966084442cd2193.image", avatar)
+	fi, err = os.Stat("/tmp/avatars.test/60/0b7f849446d3383546d15a480966084442cd2193.image")
+	assert.NoError(t, err)
+	assert.Equal(t, int64(6986), fi.Size())
+
+	p = NewFSAvatarStore("/dev/null", 300)
 	_, err = p.Put("user1", strings.NewReader("some picture bin data"))
 	assert.EqualError(t, err, "can't create file /dev/null/30/b3daa77b4c04a9551b8781d03191fe098f325e67.image: open /dev/null/30/b3daa77b4c04a9551b8781d03191fe098f325e67.image: not a directory")
 }
 
 func TestAvatarStore_Get(t *testing.T) {
-	p := NewFSAvatarStore("/tmp/avatars.test")
+	p := NewFSAvatarStore("/tmp/avatars.test", 300)
 	os.MkdirAll("/tmp/avatars.test/30", 0700)
 	defer os.RemoveAll("/tmp/avatars.test")
 	ioutil.WriteFile("/tmp/avatars.test/30/b3daa77b4c04a9551b8781d03191fe098f325e67.image", []byte("something"), 0666)
@@ -48,7 +60,7 @@ func TestAvatarStore_Get(t *testing.T) {
 }
 
 func TestAvatarStore_Location(t *testing.T) {
-	p := NewFSAvatarStore("/tmp/avatars.test")
+	p := NewFSAvatarStore("/tmp/avatars.test", 300)
 
 	tbl := []struct {
 		id  string
@@ -61,5 +73,46 @@ func TestAvatarStore_Location(t *testing.T) {
 
 	for i, tt := range tbl {
 		assert.Equal(t, tt.res, p.location(tt.id), "test #%d", i)
+	}
+}
+
+func TestAvatarStore_resize(t *testing.T) {
+	// Negative limit error.
+	resized, err := resize([]byte{}, -1)
+	assert.Nil(t, resized)
+	assert.EqualError(t, err, "limit should be greater than 0")
+	// Decode error.
+	resized, err = resize([]byte{}, 100)
+	assert.Nil(t, resized)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "can't decode avatar image")
+	cases := []struct {
+		file   string
+		wr, hr int
+	}{
+		{"testdata/circles.png", 400, 300}, // full size: 800x600 px
+		{"testdata/circles.jpg", 300, 400}, // full size: 600x800 px
+	}
+
+	for _, c := range cases {
+		img, err := ioutil.ReadFile(c.file)
+		require.Nil(t, err, "can't open test file %s", c.file)
+
+		// No need for resize, avatar dimentions are smaller than resize limit.
+		resized, err = resize(img, 800)
+		assert.Nilf(t, err, "file %s", c.file)
+		assert.Nilf(t, resized, "file %s", c.file)
+
+		// Resizing to half of width. Check resized avatar format PNG.
+		resized, err = resize(img, 400)
+		assert.Nilf(t, err, "file %s", c.file)
+		assert.NotNilf(t, resized, "file %s", c.file)
+
+		imgRz, format, err := image.Decode(bytes.NewReader(resized))
+		assert.Nilf(t, err, "file %s", c.file)
+		assert.Equalf(t, "png", format, "file %s", c.file)
+		bounds := imgRz.Bounds()
+		assert.Equalf(t, c.wr, bounds.Dx(), "file %s", c.file)
+		assert.Equalf(t, c.hr, bounds.Dy(), "file %s", c.file)
 	}
 }
