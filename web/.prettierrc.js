dec8bb4fc929e25f672ac5014115d4a2270d4af0
Efremov Alexey <lexich121@gmail.com>
2018-07-03 11:15:31 +0300

    Add: Issue #92 prettier + husky + lint-staged

---
+++ web/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 120,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+};
