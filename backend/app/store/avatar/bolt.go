0839a8a8802f8266567e14b14e35a8c5a6b545c0
Umputun <umputun@gmail.com>
2018-09-14 19:21:36 -0500

    separate bucket for avatars metas

--- backend/app/store/avatar/bolt.go
+++ backend/app/store/avatar/bolt.go
@@ -22,7 +22,8 @@ type BoltDB struct {
 	db          *bolt.DB
 }
 
-const bktName = "avatars"
+const avatarsBktName = "avatars"
+const metasBktName = "metas"
 
 // NewBoltDB makes bolt avatar store
 func NewBoltDB(fileName string, options bolt.Options, resizeLimit int) (*BoltDB, error) {
@@ -31,11 +32,14 @@ func NewBoltDB(fileName string, options bolt.Options, resizeLimit int) (*BoltDB,
 		return nil, errors.Wrapf(err, "failed to make boltdb for %s", fileName)
 	}
 	err = db.Update(func(tx *bolt.Tx) error {
-		_, e := tx.CreateBucketIfNotExists([]byte(bktName))
-		return errors.Wrapf(e, "failed to create top level bucket %s", bktName)
+		if _, e := tx.CreateBucketIfNotExists([]byte(avatarsBktName)); e != nil {
+			return errors.Wrapf(e, "failed to create top level bucket %s", avatarsBktName)
+		}
+		_, e := tx.CreateBucketIfNotExists([]byte(metasBktName))
+		return errors.Wrapf(e, "failed to create top metas bucket %s", metasBktName)
 	})
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to initialize boltdb db bucket %s for %s", bktName, fileName)
+		return nil, errors.Wrapf(err, "failed to initialize boltdb db %q buckets", fileName)
 	}
 	return &BoltDB{db: db, fileName: fileName, resizeLimit: resizeLimit}, nil
 }
@@ -55,8 +59,11 @@ func (b *BoltDB) Put(userID string, reader io.Reader) (avatar string, err error)
 		if _, err = io.Copy(buf, reader); err != nil {
 			return errors.Wrapf(err, "can't read avatar %s", avatarID)
 		}
-		return errors.Wrapf(tx.Bucket([]byte(bktName)).Put([]byte(avatarID), buf.Bytes()),
-			"can't put to bucket with %s", avatarID)
+
+		if err = tx.Bucket([]byte(avatarsBktName)).Put([]byte(avatarID), buf.Bytes()); err != nil {
+			return errors.Wrapf(err, "can't put to bucket with %s", avatarID)
+		}
+		return tx.Bucket([]byte(metasBktName)).Put([]byte(avatarID), []byte(b.sha1(buf.Bytes(), id)))
 	})
 	return avatarID, err
 }
@@ -65,7 +72,7 @@ func (b *BoltDB) Put(userID string, reader io.Reader) (avatar string, err error)
 func (b *BoltDB) Get(avatarID string) (reader io.ReadCloser, size int, err error) {
 	buf := &bytes.Buffer{}
 	err = b.db.View(func(tx *bolt.Tx) error {
-		data := tx.Bucket([]byte(bktName)).Get([]byte(avatarID))
+		data := tx.Bucket([]byte(avatarsBktName)).Get([]byte(avatarID))
 		if data == nil {
 			return errors.Errorf("can't load avatar %s", avatarID)
 		}
@@ -77,34 +84,33 @@ func (b *BoltDB) Get(avatarID string) (reader io.ReadCloser, size int, err error
 
 // ID returns a fingerprint of the avatar content.
 func (b *BoltDB) ID(avatarID string) (id string) {
-	reader, size, err := b.Get(avatarID)
+	data := []byte{}
+	err := b.db.View(func(tx *bolt.Tx) error {
+		data = tx.Bucket([]byte(metasBktName)).Get([]byte(avatarID))
+		if data == nil {
+			return errors.Errorf("can't load avatar's id for %s", avatarID)
+		}
+		return nil
+	})
 	if err != nil {
 		log.Printf("[DEBUG] can't get avatar info '%s', %s", avatarID, err)
 		return store.EncodeID(avatarID)
 	}
-	data := make([]byte, size)
-	if _, e := reader.Read(data); e != nil {
-		log.Printf("[DEBUG] can't read avatar data '%s', %s", avatarID, err)
-		return store.EncodeID(avatarID)
-	}
-
-	h := sha1.New()
-	if _, err = h.Write(data); err != nil {
-		log.Printf("[DEBUG] can't apply sha1 for content of '%s', %s", avatarID, err)
-		return store.EncodeID(avatarID)
-	}
-
-	return hex.EncodeToString(h.Sum(nil))
+	return string(data)
 }
 
 // Remove avatar from bolt
 func (b *BoltDB) Remove(avatarID string) (err error) {
 	return b.db.Update(func(tx *bolt.Tx) error {
-		bkt := tx.Bucket([]byte(bktName))
+		bkt := tx.Bucket([]byte(avatarsBktName))
 		if bkt.Get([]byte(avatarID)) == nil {
 			return errors.Errorf("avatar key not found, %s", avatarID)
 		}
-		return errors.Wrapf(tx.Bucket([]byte(bktName)).Delete([]byte(avatarID)), "can't delete object %s", avatarID)
+		if err = tx.Bucket([]byte(avatarsBktName)).Delete([]byte(avatarID)); err != nil {
+			return errors.Wrapf(err, "can't delete avatar object %s", avatarID)
+		}
+		return errors.Wrapf(tx.Bucket([]byte(metasBktName)).Delete([]byte(avatarID)),
+			"can't delete meta object %s", avatarID)
 	})
 }
 
@@ -112,10 +118,19 @@ func (b *BoltDB) Remove(avatarID string) (err error) {
 // note: id includes .image suffix
 func (b *BoltDB) List() (ids []string, err error) {
 	err = b.db.View(func(tx *bolt.Tx) error {
-		return tx.Bucket([]byte(bktName)).ForEach(func(k, _ []byte) error {
+		return tx.Bucket([]byte(metasBktName)).ForEach(func(k, _ []byte) error {
 			ids = append(ids, string(k))
 			return nil
 		})
 	})
 	return ids, errors.Wrap(err, "failed to list")
+}
+
+func (b *BoltDB) sha1(data []byte, avatarID string) (id string) {
+	h := sha1.New()
+	if _, err := h.Write(data); err != nil {
+		log.Printf("[DEBUG] can't apply sha1 for content of '%s', %s", avatarID, err)
+		return store.EncodeID(avatarID)
+	}
+	return hex.EncodeToString(h.Sum(nil))
 }
