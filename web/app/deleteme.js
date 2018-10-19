be41daae73df299318e5ec6cf51b4536bbcd956a
Pavel Vinnyk <ur300@users.noreply.github.com>
2018-10-20 00:57:23 +0300

    Auth handling added to deleteme.html #170 (#208)

--- web/app/deleteme.js
+++ web/app/deleteme.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-import { NODE_ID } from 'common/constants';
-import { approveDeleteMe } from 'common/api';
+import { NODE_ID, BASE_URL } from 'common/constants';
+import { approveDeleteMe, getUser } from 'common/api';
 import { token } from 'common/settings';
 
 if (document.readyState === 'loading') {
@@ -16,14 +16,29 @@ function init() {
     console.error("Remark42: Can't find root node.");
     return;
   }
-  approveDeleteMe(token).then(
-    data =>
-      (node.innerHTML = `
-        <h3>User deleted successfully</h3>
-        <pre>${JSON.stringify(data, null, 4)}</pre>`),
-    err =>
-      (node.innerHTML = `
-        <h3>Something went wrong</h3>
-        <pre>${err}</pre>`)
-  );
+
+  getUser()
+    .then(user => {
+      if (!user.admin) {
+        handleNotAuthorizedError(node);
+        return;
+      }
+
+      approveDeleteMe(token).then(
+        data =>
+          (node.innerHTML = `
+			<h3>User deleted successfully</h3>
+			<pre>${JSON.stringify(data, null, 4)}</pre>`),
+        err =>
+          (node.innerHTML = `
+              <h3>Something went wrong</h3>
+              <pre>${err}</pre>`)
+      );
+    })
+    .catch(() => handleNotAuthorizedError(node));
+}
+
+function handleNotAuthorizedError(node) {
+  node.innerHTML = `<h3>You are not logged in</h3>
+        			  <p><a href='${BASE_URL}' target='_blank'>Sign in</a> as admin to delete user information</p>`;
 }
