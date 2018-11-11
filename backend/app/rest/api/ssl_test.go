d67ac5c513d13891ec40b0479cb5eb7487913482
a.kosourov <a.kosourov@bankex.com>
2018-11-11 14:18:38 +0300

    add test for http challenge

--- backend/app/rest/api/ssl_test.go
+++ backend/app/rest/api/ssl_test.go
@@ -1,13 +1,22 @@
 package api
 
 import (
+	"context"
 	"crypto/tls"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+	"time"
 
+	bolt "github.com/coreos/bbolt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	adminstore "github.com/umputun/remark/backend/app/store/admin"
+	"github.com/umputun/remark/backend/app/store/engine"
+	"github.com/umputun/remark/backend/app/store/service"
 )
 
 func TestSSL_Redirect(t *testing.T) {
@@ -34,4 +43,68 @@ func TestSSL_Redirect(t *testing.T) {
 	defer resp.Body.Close()
 	assert.Equal(t, 307, resp.StatusCode)
 	assert.Equal(t, "https://localhost:443/blah?param=1", resp.Header.Get("Location"))
+}
+
+func TestSSL_ACME_HTTPChallengeRouter(t *testing.T) {
+	rest := Rest{
+		RemarkURL: "https://localhost:443",
+		SSLConfig: SSLConfig{
+			ACMELocation: "acme",
+		},
+		DataService: prepDataService(t), // for getting admin email
+	}
+
+	m := rest.makeAutocertManager()
+	defer os.RemoveAll(rest.SSLConfig.ACMELocation)
+
+	ts := httptest.NewServer(rest.httpChallengeRouter(m))
+	defer ts.Close()
+
+	client := http.Client{
+		// prevent http redirect
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+	}
+
+	// check http to https redirect response
+	resp, err := client.Get(ts.URL + "/blah?param=1")
+	require.Nil(t, err)
+	defer resp.Body.Close()
+	assert.Equal(t, 307, resp.StatusCode)
+	assert.Equal(t, "https://localhost:443/blah?param=1", resp.Header.Get("Location"))
+
+	// check acme http challenge
+	req, err := http.NewRequest("GET", ts.URL+"/.well-known/acme-challenge/token123", nil)
+	require.Nil(t, err)
+	req.Host = "localhost" // for passing hostPolicy check
+	resp, err = client.Do(req)
+	require.Nil(t, err)
+	defer resp.Body.Close()
+	assert.Equal(t, 404, resp.StatusCode)
+
+	err = m.Cache.Put(context.Background(), "token123+http-01", []byte("token"))
+	assert.Nil(t, err)
+
+	resp, err = client.Do(req)
+	require.Nil(t, err)
+	defer resp.Body.Close()
+	assert.Equal(t, 200, resp.StatusCode)
+	body, err := ioutil.ReadAll(resp.Body)
+	require.Nil(t, err)
+	assert.Equal(t, "token", string(body))
+}
+
+func prepDataService(t *testing.T) *service.DataStore {
+	b, err := engine.NewBoltDB(bolt.Options{}, engine.BoltSite{FileName: testDb, SiteID: "radio-t"})
+	require.Nil(t, err)
+
+	adminStore := adminstore.NewStaticStore("123456", []string{"a1", "a2"}, "admin@remark-42.com")
+
+	return &service.DataStore{
+		Interface:      b,
+		EditDuration:   5 * time.Minute,
+		MaxCommentSize: 4000,
+		AdminStore:     adminStore,
+	}
 }
