8e48605b75d0205dabf13bc4c631e610fd3e7d72
Vyrtsev Mikhail <reeywhaar@gmail.com>
2018-11-14 21:20:41 +0300

    oh no

--- web/app/common/localStorage.js
+++ web/app/common/localStorage.js
@@ -1,21 +1,21 @@
-import { isAvailable } from 'common/constants';
+import { IS_STORAGE_AVAILABLE } from 'common/constants';
 
 const failMessage = 'remark42: localStorage access denied, check browser preferences';
 
-export const setItem = isAvailable
+export const setItem = IS_STORAGE_AVAILABLE
   ? localStorage.setItem.bind(localStorage)
   : () => {
       console.error(failMessage); // eslint-disable-line no-console
     };
 
-export const getItem = isAvailable
+export const getItem = IS_STORAGE_AVAILABLE
   ? localStorage.getItem.bind(localStorage)
   : () => {
       console.error(failMessage); // eslint-disable-line no-console
       return null;
     };
 
-export const removeItem = isAvailable
+export const removeItem = IS_STORAGE_AVAILABLE
   ? localStorage.removeItem.bind(localStorage)
   : () => {
       console.error(failMessage); // eslint-disable-line no-console
