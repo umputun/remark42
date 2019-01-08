e5c8ac18de454cfeee7214d6031a5494419e8f63
Umputun <umputun@gmail.com>
2019-01-08 12:44:07 -0600

    bad import

--- backend/app/rest/api/rss_test.go
+++ backend/app/rest/api/rss_test.go
@@ -7,8 +7,9 @@ import (
 	"testing"
 	"time"
 
-	"github.com/go-pkgz/mongo/.vendor/github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
 	"github.com/umputun/remark/backend/app/store"
 )
 
