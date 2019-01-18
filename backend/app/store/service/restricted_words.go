845508e2408f6da05ad7f558523deb65bbb74071
Aleksandr Melnikov <a.melnikov@mylivn.com>
2019-01-18 15:44:39 +0100

    prepared interfaces and naive implementation for restricted words

---
+++ backend/app/store/service/restricted_words.go
@@ -0,0 +1,110 @@
+package service
+
+import (
+	"strings"
+	"unicode"
+)
+
+type Tokenizer interface {
+	Tokenize(text string) []string
+}
+
+type WhitespaceTokenizer struct{}
+
+func (_ WhitespaceTokenizer) Tokenize(text string) []string {
+	tokens := make([]string, 0, 10) // accumulator for tokens
+	word := false                   // flag shows if current range is word
+	start := 0                      // beginning of the current range
+	lastPos := len(text) - 1        // last position in text string
+
+	for pos, r := range text {
+		if unicode.IsLetter(r) || unicode.IsNumber(r) {
+			if !word {
+				// everything from start to pos - 1 is not a word, so reset start and start word tracking
+				start = pos
+				word = true
+			}
+
+			if pos == lastPos {
+				// since we append tokens when we already left the word (on next iteration),
+				// we need to do it manually for the last iteration
+				tokens = append(tokens, strings.ToLower(text[start:pos+1]))
+				break
+			}
+
+			continue
+		} else if word && start < pos {
+			// everything from start to pos - 1 is a word, so add it as a token and reset start
+			tokens = append(tokens, strings.ToLower(text[start:pos]))
+			start = pos
+		}
+
+		// exited the word
+		word = false
+	}
+
+	return tokens
+}
+
+type Matcher interface {
+	Match(words []string) bool
+}
+
+type SimpleMatcher struct {
+	restricted map[string]bool
+}
+
+func NewSimpleMatcher(words []string) *SimpleMatcher {
+	restricted := make(map[string]bool)
+	for _, w := range words {
+		restricted[strings.ToLower(w)] = true
+	}
+
+	return &SimpleMatcher{restricted}
+}
+
+func (l *SimpleMatcher) Match(words []string) bool {
+	for _, w := range words {
+		_, present := l.restricted[w]
+		if present {
+			return true
+		}
+	}
+	return false
+}
+
+type Lister interface {
+	List(siteID string) (restricted []string, err error)
+}
+
+type Checker interface {
+	Check(siteID string, text string) bool
+}
+
+type RestrictedWordsChecker struct {
+	tokenizer      Tokenizer
+	lister         Lister
+	matcherFactory func([]string) Matcher
+	matchers       map[string]Matcher
+}
+
+func NewRestrictedWordsChecker(tokenizer Tokenizer, lister Lister, matcherFactory func([]string) Matcher) *RestrictedWordsChecker {
+	return &RestrictedWordsChecker{tokenizer, lister, matcherFactory, make(map[string]Matcher)}
+}
+
+func (c *RestrictedWordsChecker) Check(siteID string, text string) bool {
+	tokens := c.tokenizer.Tokenize(text)
+
+	matcher, _ := c.matchers[siteID]
+	if matcher == nil {
+		words, err := c.lister.List(siteID)
+		if err != nil {
+			// todo log error
+			return false
+		}
+		matcher = c.matcherFactory(words)
+		c.matchers[siteID] = matcher
+	}
+
+	return matcher.Match(tokens)
+}
---
+++ backend/app/store/service/restricted_words_test.go
@@ -0,0 +1,50 @@
+package service
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestWhitespaceTokenizer_Tokenize(t *testing.T) {
+	tokenizer := WhitespaceTokenizer{}
+	input := "   word0 word1 word2, word3,,, !word4 !word5? word6-word7 word8"
+	expectedTokens := []string{"word0", "word1", "word2", "word3", "word4", "word5", "word6", "word7", "word8"}
+	actualTokens := tokenizer.Tokenize(input)
+
+	assert.Equal(t, expectedTokens, actualTokens)
+}
+
+func TestWhitespaceTokenizer_TokenizeLanguages(t *testing.T) {
+	tokenizer := WhitespaceTokenizer{}
+	input := "русский 中文 ไทย française"
+	expectedTokens := []string{"русский", "中文", "ไทย", "française"}
+	actualTokens := tokenizer.Tokenize(input)
+
+	assert.Equal(t, expectedTokens, actualTokens)
+}
+
+func TestWhitespaceTokenizer_TokenizeSingleWord(t *testing.T) {
+	tokenizer := WhitespaceTokenizer{}
+	expectedTokens := []string{"word"}
+	actualTokens := tokenizer.Tokenize("word")
+	assert.Equal(t, 1, len(actualTokens))
+	assert.Equal(t, expectedTokens, actualTokens)
+}
+
+func TestWhitespaceTokenizer_TokenizeEmptyString(t *testing.T) {
+	tokenizer := WhitespaceTokenizer{}
+	actualTokens := tokenizer.Tokenize("")
+	assert.Equal(t, 0, len(actualTokens))
+}
+
+func TestWhitespaceTokenizer_TokenizeSemanticallyEmptyString(t *testing.T) {
+	tokenizer := WhitespaceTokenizer{}
+	actualTokens := tokenizer.Tokenize("\t\t\n\t \n\t \r\n    \t ,,, !#$%^&*()")
+	assert.Equal(t, 0, len(actualTokens))
+}
+
+func TestWhitespaceTokenizer_TokenizeEmoji(t *testing.T) {
+	tokenizer := WhitespaceTokenizer{}
+	actualTokens := tokenizer.Tokenize("👍")
+	assert.Equal(t, 0, len(actualTokens))
+}
