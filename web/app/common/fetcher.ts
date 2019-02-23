2be69daf901108717851d22b5dd3f0c5f74dcbd4
Vyrtsev Mikhail <reeywhaar@gmail.com>
2019-02-23 22:23:05 +0300

    remove noizy typing

--- web/app/common/fetcher.ts
+++ web/app/common/fetcher.ts
@@ -16,53 +16,51 @@ type FetcherInit =
     };
 
 const fetcher = methods.reduce(
-  (acc: Partial<{ [K in FetcherMethod]: (data: FetcherInit) => Promise<unknown> }>, method: FetcherMethod) => {
+  (acc, method) => {
     acc[method] = <T = unknown>(data: FetcherInit): Promise<T> => {
       const { url, body = undefined, withCredentials = false, overriddenApiBase = API_BASE } =
         typeof data === 'string' ? { url: data } : data;
       const basename = `${BASE_URL}${overriddenApiBase}`;
 
-      {
-        const headers = new Headers({
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'X-XSRF-TOKEN': getCookie('XSRF-TOKEN') || '',
-        });
+      const headers = new Headers({
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-XSRF-TOKEN': getCookie('XSRF-TOKEN') || '',
+      });
 
-        let rurl = `${basename}${url}`;
+      let rurl = `${basename}${url}`;
 
-        const parameters: RequestInit = {
-          method,
-          headers,
-          mode: 'cors',
-          credentials: withCredentials ? 'include' : 'omit',
-        };
+      const parameters: RequestInit = {
+        method,
+        headers,
+        mode: 'cors',
+        credentials: withCredentials ? 'include' : 'omit',
+      };
 
-        if (body) {
-          if (typeof body === 'object') {
-            parameters.body = JSON.stringify(body);
-          } else {
-            parameters.body = body;
-          }
+      if (body) {
+        if (typeof body === 'object') {
+          parameters.body = JSON.stringify(body);
+        } else {
+          parameters.body = body;
         }
-
-        if (siteId && method !== 'post' && !rurl.includes('?site=') && !rurl.includes('&site=')) {
-          rurl += (rurl.includes('?') ? '&' : '?') + `site=${siteId}`;
-        }
-
-        return fetch(rurl, parameters).then(res => {
-          const date = (res.headers.has('date') && res.headers.get('date')) || '';
-          const timestamp = isNaN(Date.parse(date)) ? 0 : Date.parse(date);
-          const timeDiff = (new Date().getTime() - timestamp) / 1000;
-          StaticStore.serverClientTimeDiff = timeDiff;
-
-          return res.json();
-        });
       }
+
+      if (siteId && method !== 'post' && !rurl.includes('?site=') && !rurl.includes('&site=')) {
+        rurl += (rurl.includes('?') ? '&' : '?') + `site=${siteId}`;
+      }
+
+      return fetch(rurl, parameters).then(res => {
+        const date = (res.headers.has('date') && res.headers.get('date')) || '';
+        const timestamp = isNaN(Date.parse(date)) ? 0 : Date.parse(date);
+        const timeDiff = (new Date().getTime() - timestamp) / 1000;
+        StaticStore.serverClientTimeDiff = timeDiff;
+
+        return res.json();
+      });
     };
     return acc;
   },
-  {}
-) as { [K in FetcherMethod]: <T = unknown>(data: FetcherInit) => Promise<T> };
+  {} as { [K in FetcherMethod]: <T = unknown>(data: FetcherInit) => Promise<T> }
+);
 
 export default fetcher;
