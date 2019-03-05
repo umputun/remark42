6f163fe01f85275f45dd86304980ce562b5e63ee
Vyrtsev Mikhail <reeywhaar@gmail.com>
2019-03-06 02:18:32 +0300

    remove one more type assertion

--- web/app/common/fetcher.ts
+++ web/app/common/fetcher.ts
@@ -17,69 +17,65 @@ type FetcherInit =
 
 type FetcherObject = { [K in FetcherMethod]: <T = unknown>(data: FetcherInit) => Promise<T> };
 
-const fetcher: FetcherObject = methods.reduce<FetcherObject>(
-  (acc, method) => {
-    acc[method] = <T = unknown>(data: FetcherInit): Promise<T> => {
-      const { url, body = undefined, withCredentials = false, overriddenApiBase = API_BASE } =
-        typeof data === 'string' ? { url: data } : data;
-      const basename = `${BASE_URL}${overriddenApiBase}`;
+const fetcher = methods.reduce<Partial<FetcherObject>>((acc, method) => {
+  acc[method] = <T = unknown>(data: FetcherInit): Promise<T> => {
+    const { url, body = undefined, withCredentials = false, overriddenApiBase = API_BASE } =
+      typeof data === 'string' ? { url: data } : data;
+    const basename = `${BASE_URL}${overriddenApiBase}`;
 
-      const headers = new Headers({
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'X-XSRF-TOKEN': getCookie('XSRF-TOKEN') || '',
-      });
+    const headers = new Headers({
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      'X-XSRF-TOKEN': getCookie('XSRF-TOKEN') || '',
+    });
 
-      let rurl = `${basename}${url}`;
+    let rurl = `${basename}${url}`;
 
-      const parameters: RequestInit = {
-        method,
-        headers,
-        mode: 'cors',
-        credentials: withCredentials ? 'include' : 'omit',
-      };
-
-      if (body) {
-        if (typeof body === 'object') {
-          parameters.body = JSON.stringify(body);
-        } else {
-          parameters.body = body;
-        }
-      }
-
-      if (siteId && method !== 'post' && !rurl.includes('?site=') && !rurl.includes('&site=')) {
-        rurl += (rurl.includes('?') ? '&' : '?') + `site=${siteId}`;
-      }
-
-      return fetch(rurl, parameters).then(res => {
-        const date = (res.headers.has('date') && res.headers.get('date')) || '';
-        const timestamp = isNaN(Date.parse(date)) ? 0 : Date.parse(date);
-        const timeDiff = (new Date().getTime() - timestamp) / 1000;
-        StaticStore.serverClientTimeDiff = timeDiff;
-
-        if (res.status >= 400) {
-          return res.text().then(text => {
-            let err;
-            try {
-              err = JSON.parse(text);
-            } catch (e) {
-              throw text;
-            }
-            throw err;
-          });
-        }
-
-        if (res.headers.has('Content-Type') && res.headers.get('Content-Type')!.indexOf('application/json') === 0) {
-          return res.json();
-        }
-
-        return res.text();
-      });
+    const parameters: RequestInit = {
+      method,
+      headers,
+      mode: 'cors',
+      credentials: withCredentials ? 'include' : 'omit',
     };
-    return acc;
-  },
-  // eslint-disable-next-line
-  {} as FetcherObject
-);
+
+    if (body) {
+      if (typeof body === 'object') {
+        parameters.body = JSON.stringify(body);
+      } else {
+        parameters.body = body;
+      }
+    }
+
+    if (siteId && method !== 'post' && !rurl.includes('?site=') && !rurl.includes('&site=')) {
+      rurl += (rurl.includes('?') ? '&' : '?') + `site=${siteId}`;
+    }
+
+    return fetch(rurl, parameters).then(res => {
+      const date = (res.headers.has('date') && res.headers.get('date')) || '';
+      const timestamp = isNaN(Date.parse(date)) ? 0 : Date.parse(date);
+      const timeDiff = (new Date().getTime() - timestamp) / 1000;
+      StaticStore.serverClientTimeDiff = timeDiff;
+
+      if (res.status >= 400) {
+        return res.text().then(text => {
+          let err;
+          try {
+            err = JSON.parse(text);
+          } catch (e) {
+            throw text;
+          }
+          throw err;
+        });
+      }
+
+      if (res.headers.has('Content-Type') && res.headers.get('Content-Type')!.indexOf('application/json') === 0) {
+        return res.json();
+      }
+
+      return res.text();
+    });
+  };
+  return acc;
+}, {}) as FetcherObject;
 
 export default fetcher;
