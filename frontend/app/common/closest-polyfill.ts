f16f9761a62bdef91a9ec61f3639ef62d9f2a329
Vyrtsev Mikhail <reeywhaar@gmail.com>
2019-05-10 13:50:21 +0300

    convert closest-polyfill to ts

---
+++ frontend/app/common/closest-polyfill.ts
@@ -0,0 +1,17 @@
+if (!Element.prototype.matches) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Element.prototype.matches = (Element.prototype as any).msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s: string) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let el: any = this;
+
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
+}
