6b6ea69dd6653bc44b2a49a432a678d8bd82049d
Dmitry Verkhoturov <paskal.07@gmail.com>
2019-07-03 03:04:12 +0200

    implement Email.Send, basic test of sending email

--- backend/app/notify/email.go
+++ backend/app/notify/email.go
@@ -27,12 +27,13 @@ type Email struct {
 	password  string
 	keepAlive time.Duration
 	sendChan  chan *gomail.Message
+	errChan   chan error
 }
 
 const emailConnectionKeepAlive = 30 * time.Second
 
 //NewEmail makes email object for notifications and run sending daemon
-func NewEmail(params EmailParams) (*Email, error) {
+func NewEmail(ctx context.Context, params EmailParams) (*Email, error) {
 
 	res := Email{
 		server:    params.Server,
@@ -41,6 +42,7 @@ func NewEmail(params EmailParams) (*Email, error) {
 		password:  params.Password,
 		keepAlive: params.KeepAlive,
 		sendChan:  make(chan *gomail.Message),
+		errChan:   make(chan error),
 	}
 
 	if res.keepAlive == 0 {
@@ -51,34 +53,57 @@ func NewEmail(params EmailParams) (*Email, error) {
 		"[DEBUG] create new email notifier for server %s with user %s, keepalive=%s",
 		res.server, res.username, res.keepAlive)
 
-	// test connection before starting a daemon
+	// Test connection before starting a daemon.
 	tmpConn, err := gomail.NewDialer(res.server, res.port, res.username, res.password).Dial()
 	if err != nil {
 		return &res, errors.Errorf(
-			"[WARN] error connecting to '%s':%d with username '%s': %s",
+			"[WARN] error establishing test connecting to '%s':%d with username '%s': %s",
 			res.server, res.port, res.username, err)
 	}
-	err = tmpConn.Close()
-	if err != nil {
+	if err = tmpConn.Close(); err != nil {
 		return &res, errors.Errorf(
-			"[WARN] error closing connection to %s:%d: %s",
+			"[WARN] error closing test connection to %s:%d: %s",
 			res.server, res.port, err)
 	}
 
+	// Activate server goroutine.
 	go res.activate()
-
-	// TODO: do we need to close this?
-	// Close the channel to stop the mail daemon.
-	//close(res.sendChan)
+	// Close the channel to stop the mail daemon
+	go func() { <-ctx.Done(); close(res.sendChan) }()
 
 	return &res, nil
 }
 
-// Send email
+// Send email from request to address in settings
 func (e *Email) Send(ctx context.Context, req request) error {
 	log.Printf("[DEBUG] send notification via %s, comment id %s", e, req.comment.ID)
-	// TODO: write actual code
-	return nil
+
+	// Gather data for message to send from request.
+	from := req.comment.User.Name
+	if req.comment.ParentID != "" {
+		from += " → " + req.parent.User.Name
+	}
+	link := fmt.Sprintf("↦ [original comment](%s)", req.comment.Locator.URL+uiNav+req.comment.ID)
+	if req.comment.PostTitle != "" {
+		link = fmt.Sprintf("↦ [%s](%s)", req.comment.PostTitle, req.comment.Locator.URL+uiNav+req.comment.ID)
+	}
+	body := fmt.Sprintf("%s\n\n%s\n\n%s", from, req.comment.Orig, link)
+
+	// Create message.
+	m := gomail.NewMessage()
+	// TODO: figure out where "from" and "to" addresses come from
+	//m.SetHeader("From", "no-reply@example.com")
+	//m.SetAddressHeader("To", req.Address, req.Name)
+	m.SetHeader("Subject", fmt.Sprintf("New comment for \"%s\"", req.comment.PostTitle))
+	m.SetBody("text/html", body)
+
+	// Wait for ability to send message and return error from error channel after sending it.
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case e.sendChan <- m:
+		return <-e.errChan
+	}
 }
 
 func (e *Email) String() string {
@@ -95,28 +120,35 @@ func (e *Email) activate() {
 		select {
 		case m, ok := <-e.sendChan:
 			if !ok {
+				close(e.errChan)
 				return
 			}
 			if !open {
 				if s, err = d.Dial(); err != nil {
-					log.Printf(
-						"[WARN] error connecting to %s:%d with username %s: %s",
+					// Problems with connection, returning error and considering connection not established.
+					e.errChan <- errors.Errorf(
+						"error connecting to %s:%d with username %s: %s",
 						e.server, e.port, e.username, err)
+					break
 				}
 				open = true
 			}
-			if err := gomail.Send(s, m); err != nil {
-				log.Printf(
-					"[INFO] error sending to %s:%d with username %s: %s",
-					e.server, e.port, e.username, err)
+			if err = gomail.Send(s, m); err != nil {
+				e.errChan <- errors.Errorf(
+					"error sending to %s:%d: %s",
+					e.server, e.port, err)
 			}
+			// Email sent successfully.
+			e.errChan <- nil
 		// Close the connection to the SMTP server if no email was sent in the keepAlive period.
 		case <-time.After(e.keepAlive):
 			if open {
 				if err := s.Close(); err != nil {
+					// Problems with closing connection, considering connection still established.
 					log.Printf(
 						"[WARN] error closing connection with %s:%d with username %s: %s",
 						e.server, e.port, e.username, err)
+					break
 				}
 				open = false
 			}
--- backend/app/notify/email_test.go
+++ backend/app/notify/email_test.go
@@ -1,14 +1,18 @@
 package notify
 
 import (
+	"context"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/umputun/remark/backend/app/store"
 )
 
 func TestEmail(t *testing.T) {
-	email, err := NewEmail(EmailParams{})
+	// Test failed start of the server.
+	email, err := NewEmail(context.Background(), EmailParams{})
 	assert.Error(t, err, "No connection established with empty address and port zero")
 	assert.NotNil(t, email, "despite the error we got object reference")
 	assert.Equal(t, email.server, "")
@@ -17,4 +21,18 @@ func TestEmail(t *testing.T) {
 	assert.Equal(t, email.password, "")
 	assert.Equal(t, email.keepAlive, 30*time.Second, "default value if keepAlive is not defined is set to 30s")
 	assert.Equal(t, "email: ''@'':0", email.String())
+
+	c := store.Comment{Text: "some text", ParentID: "1"}
+	c.User.Name = "from"
+	c.PostTitle = "post title"
+	cp := store.Comment{Text: "some parent text"}
+	cp.User.Name = "to"
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() { cancel(); <-ctx.Done() }()
+	assert.EqualError(t, email.Send(ctx, request{comment: c, parent: cp}), "context canceled")
+
+	go func() { _ = email.Send(context.Background(), request{comment: c, parent: cp}) }()
+	message := <-email.sendChan
+	assert.Equal(t, message.GetHeader("Subject"), []string{"New comment for \"post title\""})
 }
