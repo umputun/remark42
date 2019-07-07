e0ba7564f1326796e71e42e4c6e2dffcd9b6d51d
Vyrtsev Mikhail <reeywhaar@gmail.com>
2019-07-07 21:19:08 +0300

    add bench util

---
+++ frontend/app/utils/bench.ts
@@ -0,0 +1,8 @@
+export function bench<T>(fn: () => T, label: string = 'bench'): T {
+  const d = performance.now();
+  const r = fn();
+  const dd = performance.now();
+  // eslint-disable-next-line no-console
+  console.info(label, dd - d);
+  return r;
+}
