c769913ec8e80be8774a8d067fe6972145d8547d
Dmitry Verkhoturov <paskal.07@gmail.com>
2019-07-14 20:35:08 +0200

    use template for message generation, deduplicate code

--- backend/app/notify/email.go
+++ backend/app/notify/email.go
@@ -1,14 +1,17 @@
 package notify
 
 import (
+	"bytes"
 	"context"
 	"crypto/tls"
 	"fmt"
 	"net"
 	"net/smtp"
+	"text/template"
 	"time"
 
 	log "github.com/go-pkgz/lgr"
+	"github.com/go-pkgz/repeater"
 	"github.com/pkg/errors"
 )
 
@@ -21,18 +24,23 @@ type EmailParams struct {
 	Username string        // user name
 	Password string        // password
 	TimeOut  time.Duration // TLS connection timeout
+	Template string        // request message template
 }
 
 // Email implements notify.Destination for email
 type Email struct {
 	connection *smtp.Client
-	host       string        // SMTP host
-	port       int           // SMTP port
-	tls        bool          // TLS auth
-	from       string        // From email field
-	username   string        // user name
-	password   string        // password
-	timeOut    time.Duration // TLS connection timeout
+	params     EmailParams
+	template   *template.Template // request message template
+}
+
+// tmplData store data for message from request template execution
+type tmplData struct {
+	From      string
+	To        string
+	Orig      string
+	Link      string
+	PostTitle string
 }
 
 const emailConnectionTimeOut = 10 * time.Second
@@ -40,22 +48,23 @@ const emailConnectionTimeOut = 10 * time.Second
 //NewEmail makes email object for notifications and run sending daemon
 func NewEmail(params EmailParams) (*Email, error) {
 
-	res := Email{
-		host:     params.Host,
-		port:     params.Port,
-		tls:      params.TLS,
-		from:     params.From,
-		username: params.Username,
-		password: params.Password,
-		timeOut:  params.TimeOut,
-	}
-	if res.timeOut == 0 {
-		res.timeOut = emailConnectionTimeOut
+	res := Email{params: params}
+	if res.params.TimeOut == 0 {
+		res.params.TimeOut = emailConnectionTimeOut
 	}
 	log.Printf("[DEBUG] create new email notifier for server %s with user %s, timeout=%s",
-		res.host, res.username, res.timeOut)
+		res.params.Host, res.params.Username, res.params.TimeOut)
 
 	var err error
+
+	tmpl := msgTemplate
+	if params.Template != "" {
+		tmpl = params.Template
+	}
+	res.template, err = template.New("messageFromRequest").Parse(tmpl)
+	if err != nil {
+		return &res, errors.Wrapf(err, "can't parse message template")
+	}
 	res.connection, err = res.client()
 
 	return &res, err
@@ -64,40 +73,44 @@ func NewEmail(params EmailParams) (*Email, error) {
 // Send email from request to address in settings
 func (e *Email) Send(ctx context.Context, req request) error {
 	log.Printf("[DEBUG] send notification via %s, comment id %s", e, req.comment.ID)
+	// TODO: decide where to get "to" email from
 	msg := e.buildMessageFromRequest(req, "test@localhost")
-	return e.SendEmail(msg)
+	err := repeater.NewDefault(5, time.Millisecond*250).Do(ctx, func() error {
+		return e.sendEmail(msg)
+	})
+	return err
 }
 
-// SendEmail sends prepared message
-func (e *Email) SendEmail(message string) error {
+// sendEmail sends prepared message
+func (e *Email) sendEmail(message string) error {
 	// TODO: write send logic reusing e.connection
 	return nil
 }
 
 func (e *Email) String() string {
-	return fmt.Sprintf("email: '%s'@'%s':%d", e.username, e.host, e.port)
+	return fmt.Sprintf("email: '%s'@'%s':%d", e.params.Username, e.params.Host, e.params.Port)
 }
 
 //buildMessageFromRequest generates email message based on request
 func (e *Email) buildMessageFromRequest(req request, to string) (message string) {
 	subject := "New comment"
-	link := fmt.Sprintf("↦ <a href=\"%s\">original comment</a>", req.comment.Locator.URL+uiNav+req.comment.ID)
 	if req.comment.PostTitle != "" {
-		link = fmt.Sprintf("↦ <a href=\"%s\">%s</a>", req.comment.Locator.URL+uiNav+req.comment.ID, req.comment.PostTitle)
 		subject += fmt.Sprintf(" for \"%s\"", req.comment.PostTitle)
 	}
-	from := req.comment.User.Name
-	if req.comment.ParentID != "" {
-		from += " → " + req.parent.User.Name
-	}
-	// TODO: message looks bad, review it
-	msg := fmt.Sprintf("%s\n\n%s\n\n%s", from, req.comment.Orig, link)
-	return e.BuildMessage(subject, msg, to, "text/html")
+	msg := bytes.Buffer{}
+	_ = e.template.Execute(&msg, tmplData{
+		req.comment.User.Name,
+		req.parent.User.Name,
+		req.comment.Orig,
+		req.comment.Locator.URL + uiNav + req.comment.ID,
+		req.comment.PostTitle,
+	})
+	return e.buildMessage(subject, msg.String(), to, "text/html")
 }
 
-//BuildMessage generates email message
-func (e *Email) BuildMessage(subject, msg, to, contentType string) (message string) {
-	message += fmt.Sprintf("From: %s\n", e.from)
+//buildMessage generates email message
+func (e *Email) buildMessage(subject, msg, to, contentType string) (message string) {
+	message += fmt.Sprintf("From: %s\n", e.params.From)
 	message += fmt.Sprintf("To: %s\n", to)
 	message += fmt.Sprintf("Subject: %s\n", subject)
 	if contentType != "" {
@@ -108,23 +121,23 @@ func (e *Email) BuildMessage(subject, msg, to, contentType string) (message stri
 }
 
 func (e *Email) client() (c *smtp.Client, err error) {
-	srvAddress := fmt.Sprintf("%s:%d", e.host, e.port)
-	if e.tls {
+	srvAddress := fmt.Sprintf("%s:%d", e.params.Host, e.params.Port)
+	if e.params.TLS {
 		tlsConf := &tls.Config{
 			InsecureSkipVerify: false,
-			ServerName:         e.host,
+			ServerName:         e.params.Host,
 		}
 		conn, err := tls.Dial("tcp", srvAddress, tlsConf)
 		if err != nil {
 			return nil, errors.Wrapf(err, "failed to dial smtp tls to %s", srvAddress)
 		}
-		if c, err = smtp.NewClient(conn, e.host); err != nil {
+		if c, err = smtp.NewClient(conn, e.params.Host); err != nil {
 			return nil, errors.Wrapf(err, "failed to make smtp client for %s", srvAddress)
 		}
 		return c, nil
 	}
 
-	conn, err := net.DialTimeout("tcp", srvAddress, e.timeOut)
+	conn, err := net.DialTimeout("tcp", srvAddress, e.params.TimeOut)
 	if err != nil {
 		return nil, errors.Wrapf(err, "timeout connecting to %s", srvAddress)
 	}
@@ -135,3 +148,10 @@ func (e *Email) client() (c *smtp.Client, err error) {
 	}
 	return c, nil
 }
+
+var msgTemplate = `{{.From}}{{if .To}} → {{.To}}{{end}}
+
+{{.Orig}}
+
+↦ <a href="{{.Link}}">{{if .PostTitle}}{{.PostTitle}}{{else}}original comment{{end}}</a>
+`
--- backend/app/notify/email_test.go
+++ backend/app/notify/email_test.go
@@ -11,16 +11,19 @@ import (
 func TestEmptyEmailServer(t *testing.T) {
 	// Test failed start of the server.
 	email, err := NewEmail(EmailParams{})
-	assert.Error(t, err, "No connection established with empty address and port zero")
+	assert.Error(t, err, "no connection established with empty address and port zero")
 	assert.NotNil(t, email, "despite the error we got object reference")
-	assert.Equal(t, email.host, "")
-	assert.Equal(t, email.port, 0)
-	assert.Equal(t, email.tls, false)
-	assert.Equal(t, email.from, "")
-	assert.Equal(t, email.username, "")
-	assert.Equal(t, email.password, "")
-	assert.Equal(t, email.timeOut, 10*time.Second, "default value if TimeOut is not defined is set to 10s")
-	assert.Equal(t, "email: ''@'':0", email.String())
+	assert.Nil(t, email.connection, "connection was not created due to error")
+	assert.Equal(t, 10*time.Second, email.params.TimeOut, "default value if TimeOut is not defined is set to 10s")
+	assert.NotNil(t, email.template, "default template is set")
+	assert.Equal(t, email.String(), "email: ''@'':0", "correct empty object string representation")
+	// Test broken template
+	email, err = NewEmail(EmailParams{Template: "{{"})
+	assert.Error(t, err, "error due to parsing improper template")
+	assert.NotNil(t, email, "despite the error we got object reference")
+	assert.Nil(t, email.template, "default template is not set due to error")
+	assert.Nil(t, email.connection, "connection was not created due to error")
+
 }
 
 func TestBuildMessageFromRequest(t *testing.T) {
@@ -41,7 +44,8 @@ Content-Type: text/html; charset="UTF-8";
 
 orig
 
-↦ <a href="//example.org#remark42__comment-">original comment</a>`
+↦ <a href="//example.org#remark42__comment-">original comment</a>
+`
 	assert.Equal(t, emptyTitleMessage, mgs)
 	c.ParentID = "1"
 	c.PostTitle = "post title"
@@ -58,21 +62,22 @@ Content-Type: text/html; charset="UTF-8";
 
 orig
 
-↦ <a href="//example.org#remark42__comment-">post title</a>`
+↦ <a href="//example.org#remark42__comment-">post title</a>
+`
 	mgs = email.buildMessageFromRequest(req, "test_address@example.com")
 	assert.Equal(t, filledTitleMessage, mgs)
 }
 
 func TestBuildMessage(t *testing.T) {
-	email := Email{from: "from@email"}
-	msg := email.BuildMessage("test_subj", "test_body", "recepient@email", "")
+	email := Email{params: EmailParams{From: "from@email"}}
+	msg := email.buildMessage("test_subj", "test_body", "recepient@email", "")
 	expectedMsg := `From: from@email
 To: recepient@email
 Subject: test_subj
 
 test_body`
 	assert.Equal(t, expectedMsg, msg)
-	msg = email.BuildMessage("test_subj", "test_body", "recepient@email", "text/html")
+	msg = email.buildMessage("test_subj", "test_body", "recepient@email", "text/html")
 	expectedLines := `MIME-version: 1.0;
 Content-Type: text/html; charset="UTF-8";`
 	assert.Contains(t, msg, expectedLines)
@@ -83,7 +88,7 @@ func TestConnectErrors(t *testing.T) {
 	client, err := email.client()
 	assert.Nil(t, client)
 	assert.Error(t, err, "connection with wrong settings return error")
-	email = Email{tls: true}
+	email = Email{params: EmailParams{TLS: true}}
 	client, err = email.client()
 	assert.Nil(t, client)
 	assert.Error(t, err, "TLS connection with wrong settings return error")
