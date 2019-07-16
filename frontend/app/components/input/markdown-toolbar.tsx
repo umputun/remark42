2f7c4e7e038181fc9fdb4aa999da29f670866b5e
Konstantin Krivlenia <krivlenia@gmail.com>
2019-07-16 18:27:44 +0300

    Add a button for image upload (#372)
    * #371 add image icon for toolbar
    
    * #371 add file upload handler
    
    * #371 upload image from clipboard
    
    * #371 change title and decrease size for upload button
    
    * #371 allow upload few files
    
    * #371 prevent paste text after file was uploaded in firefox

--- frontend/app/components/input/markdown-toolbar.tsx
+++ frontend/app/components/input/markdown-toolbar.tsx
@@ -7,11 +7,23 @@ import ItalicIcon from './markdown-toolbar-icons/italic-icon';
 import QuoteIcon from './markdown-toolbar-icons/quote-icon';
 import CodeIcon from './markdown-toolbar-icons/code-icon';
 import LinkIcon from './markdown-toolbar-icons/link-icon';
+import ImageIcon from './markdown-toolbar-icons/image-icon';
 import UnorderedListIcon from './markdown-toolbar-icons/unordered-list-icon';
 import OrderedListIcon from './markdown-toolbar-icons/ordered-list-icon';
 
 interface Props {
   textareaId: string;
+  uploadImages: (files: File[]) => Promise<void>;
+  allowUpload: boolean;
+}
+
+interface FileEventTarget extends EventTarget {
+  readonly files: FileList | null;
+  value: string | null;
+}
+
+interface FileInputEvent extends Event {
+  readonly currentTarget: FileEventTarget | null;
 }
 
 const boldLabel = 'Add bold text <cmd-b>';
@@ -22,8 +34,20 @@ const codeLabel = 'Insert a code';
 const linkLabel = 'Add a link <cmd-k>';
 const unorderedListLabel = 'Add a bulleted list';
 const orderedListLabel = 'Add a numbered list';
+const attachImageLabel = 'Attach the image, drag & drop or paste from clipboard';
 
 export default class MarkdownToolbar extends Component<Props> {
+  constructor(props: Props) {
+    super(props);
+    this.uploadImages = this.uploadImages.bind(this);
+  }
+  async uploadImages(e: Event) {
+    const currentTarget = (e as FileInputEvent).currentTarget;
+    if (!(this.props.allowUpload && currentTarget && currentTarget.files && currentTarget.files.length !== 0)) return;
+    const files = Array.from(currentTarget.files);
+    await this.props.uploadImages(files);
+    currentTarget.value = null;
+  }
   render(props: RenderableProps<Props>) {
     return (
       <markdown-toolbar className="input__toolbar" for={props.textareaId}>
@@ -48,6 +72,12 @@ export default class MarkdownToolbar extends Component<Props> {
           <md-link className="input__toolbar-item" title={linkLabel} aria-label={linkLabel}>
             <LinkIcon />
           </md-link>
+          {this.props.allowUpload ? (
+            <label className="input__toolbar-item" title={attachImageLabel} aria-label={attachImageLabel}>
+              <input multiple class="input__toolbar-file-input" type="file" onChange={this.uploadImages} />
+              <ImageIcon />
+            </label>
+          ) : null}
         </div>
         <div className="input__toolbar-group">
           <md-unordered-list className="input__toolbar-item" title={unorderedListLabel} aria-label={unorderedListLabel}>
