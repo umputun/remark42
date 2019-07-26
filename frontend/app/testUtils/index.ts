d14fea6520e9858188bc4a3aee2a7ed2c2836236
Vyrtsev Mikhail <reeywhaar@gmail.com>
2019-07-27 00:44:46 +0300

    remove unused test renderer

--- frontend/app/testUtils/index.ts
+++ frontend/app/testUtils/index.ts
@@ -23,21 +23,3 @@ beforeEach(() => {
     version: 'jest-test',
   };
 });
-
-export function createDomContainer(setup: (domContainer: HTMLElement) => void): void {
-  let domContainer: HTMLElement | null = null;
-  beforeAll(() => {
-    domContainer = document.createElement('div');
-    (document.body || document.documentElement).appendChild(domContainer);
-    setup(domContainer);
-  });
-
-  beforeEach(() => {
-    domContainer!.innerHTML = '';
-  });
-
-  afterAll(() => {
-    domContainer!.parentNode!.removeChild(domContainer!);
-    domContainer = null;
-  });
-}
