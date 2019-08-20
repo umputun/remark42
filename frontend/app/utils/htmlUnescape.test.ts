8686feffe38bef4ee60b64022d2e48e34a840dd9
Vyrtsev Mikhail <reeywhaar@gmail.com>
2019-08-21 02:34:39 +0300

    add htmlPartialUnescape function

--- frontend/app/utils/htmlUnescape.test.ts
+++ frontend/app/utils/htmlUnescape.test.ts
@@ -1,4 +1,4 @@
-import { htmlUnescape } from './htmlUnescape';
+import { htmlUnescape, htmlPartialUnescape } from './htmlUnescape';
 
 describe('htmlUnescape', () => {
   const data: ([string, string])[] = [
@@ -10,6 +10,21 @@ describe('htmlUnescape', () => {
   for (const example of data) {
     it(`unescapes ${example[0]} to ${example[1]}`, () => {
       expect(htmlUnescape(example[0])).toStrictEqual(example[1]);
+    });
+  }
+});
+
+describe('htmlPartialUnescape', () => {
+  const data: ([string, string])[] = [
+    ['blah &amp; &amp; &#34; &#39; 123', 'blah & & " \' 123'],
+    ['&amp;&amp;&amp;&amp;', '&&&&'],
+    ['blah & & 123 &mdash; &mdash;', 'blah & & 123 &mdash; &mdash;'],
+    ['name &lt;&gt; & \' ` "', 'name &lt;&gt; & \' ` "'],
+  ];
+
+  for (const example of data) {
+    it(`unescapes ${example[0]} to ${example[1]}`, () => {
+      expect(htmlPartialUnescape(example[0])).toStrictEqual(example[1]);
     });
   }
 });
--- frontend/app/utils/htmlUnescape.ts
+++ frontend/app/utils/htmlUnescape.ts
@@ -4,3 +4,16 @@ export function htmlUnescape(input: string): string {
   htmlDecodeElement.innerHTML = input;
   return htmlDecodeElement.childNodes.length === 0 ? '' : htmlDecodeElement.childNodes[0].nodeValue || '';
 }
+
+const replaceRegexes: ([RegExp, string])[] = [[/&#34;/g, '"'], [/&#39;/g, "'"], [/&amp;/g, '&']];
+
+/**
+ * Performs partial unescape for [", ', &] symbols
+ */
+export function htmlPartialUnescape(input: string): string {
+  let out = input;
+  for (const op of replaceRegexes) {
+    out = out.replace(op[0], op[1]);
+  }
+  return out;
+}
