12a996919d33305629b96b72b45c64d11dbb9b42
Vyrtsev Mikhail <reeywhaar@gmail.com>
2019-09-29 22:57:43 +0300

    add postMessage util

---
+++ frontend/app/utils/postMessage.ts
@@ -0,0 +1,21 @@
+import { User } from '@app/common/types';
+
+export type Message =
+  | { inited: true }
+  | {
+      isUserInfoShown: true;
+      user: User;
+    }
+  | { isUserInfoShown: false }
+  | { scrollTo: number };
+
+/**
+ * Sends message to parent window
+ *
+ * @returns request success of fail
+ */
+export default function postMessage(data: Message): boolean {
+  if (!window.parent || window.parent === window) return false;
+  window.parent.postMessage(JSON.stringify(data), '*');
+  return true;
+}
