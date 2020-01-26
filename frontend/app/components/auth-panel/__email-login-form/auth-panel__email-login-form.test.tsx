21fd33ba5741938036dcbdb77e89fc789e9e533c
Pavel Mineev <pavel@mineev.me>
2020-01-26 23:00:22 +0300

    tests

--- frontend/app/components/auth-panel/__email-login-form/auth-panel__email-login-form.test.tsx
+++ frontend/app/components/auth-panel/__email-login-form/auth-panel__email-login-form.test.tsx
@@ -4,13 +4,46 @@ import { mount } from 'enzyme';
 import { EmailLoginForm, Props, State } from './auth-panel__email-login-form';
 import { User } from '@app/common/types';
 import { sleep } from '@app/utils/sleep';
+// import { validToken } from '@app/testUtils/mocks/jwt';
 
 describe('EmailLoginForm', () => {
+  const testUser = ({} as any) as User;
+  const onSuccess = jest.fn(async () => {});
+
   it('works', async () => {
-    const testUser = ({} as any) as User;
+    const onSignIn = jest.fn(async () => testUser);
+    const sendEmailVerification = jest.fn(async () => {});
+
+    const el = mount<Props, State>(
+      <EmailLoginForm
+        sendEmailVerification={sendEmailVerification}
+        onSignIn={onSignIn}
+        onSuccess={onSuccess}
+        theme="light"
+      />
+    );
+
+    await new Promise(resolve =>
+      el.setState({ usernameValue: 'someone', addressValue: 'someone@example.com' } as State, resolve)
+    );
+
+    el.find('form').simulate('submit');
+    await sleep(100);
+    expect(sendEmailVerification).toBeCalledWith('someone', 'someone@example.com');
+    expect(el.state().verificationSent).toBe(true);
+
+    await new Promise(resolve => el.setState({ tokenValue: 'abcd' } as State, resolve));
+
+    el.find('form').simulate('submit');
+    await sleep(100);
+    expect(onSignIn).toBeCalledWith('abcd');
+    expect(onSuccess).toBeCalledWith(testUser);
+  });
+
+  it('should send form by pasting token', async () => {
     const sendEmailVerification = jest.fn(async () => {});
     const onSignIn = jest.fn(async () => testUser);
-    const onSuccess = jest.fn(async () => {});
+
     const el = mount<Props, State>(
       <EmailLoginForm
         sendEmailVerification={sendEmailVerification}
@@ -20,31 +53,12 @@ describe('EmailLoginForm', () => {
       />
     );
     await new Promise(resolve =>
-      el.setState(
-        {
-          usernameValue: 'someone',
-          addressValue: 'someone@example.com',
-        } as State,
-        resolve
-      )
+      el.setState({ usernameValue: 'someone', addressValue: 'someone@example.com' } as State, resolve)
     );
     el.find('form').simulate('submit');
     await sleep(100);
-    expect(sendEmailVerification).toBeCalledWith('someone', 'someone@example.com');
-    expect(el.state().verificationSent).toBe(true);
-
-    await new Promise(resolve =>
-      el.setState(
-        {
-          tokenValue: 'abcd',
-        } as State,
-        resolve
-      )
-    );
-
-    el.find('form').simulate('submit');
-    await sleep(100);
-    expect(onSignIn).toBeCalledWith('abcd');
-    expect(onSuccess).toBeCalledWith(testUser);
+    el.update();
+    // el.find('textarea').simulate('change', { target: { value: validToken } });
+    // expect(onSignIn).toBeCalledWith(validToken);
   });
 });
