ba2d6883fa1ec7c8b8e5286a39c3b21602bd50b3
konstantin krivlenia <krivlenia@gmail.com>
2020-02-19 13:15:01 +0300

    #10 use messages value for translate reference

--- frontend/app/components/comment/getVoteMessage.ts
+++ frontend/app/components/comment/getVoteMessage.ts
@@ -1,32 +1,32 @@
 import { IntlShape, defineMessages } from 'react-intl';
 
-defineMessages({
-  'vote.own-comment': {
+const voteMessages = defineMessages({
+  ownComment: {
     id: 'vote.own-comment',
     defaultMessage: `Can't vote for your own comment`,
   },
-  'vote.guest': {
+  guest: {
     id: 'vote.guest',
     defaultMessage: 'Sign in to vote',
   },
-  'vote.only-post-page': {
+  onlyPostPage: {
     id: 'vote.only-post-page',
     defaultMessage: `Voting allowed only on post's page`,
   },
-  'vote.readonly': {
+  readonly: {
     id: 'vote.readonly',
     defaultMessage: `Can't vote on read-only topics`,
   },
-  'vote.deleted': {
+  deleted: {
     id: 'vote.deleted',
     defaultMessage: `Can't vote for deleted comment`,
   },
-  'vote.anonymous': {
+  anonymous: {
     id: 'vote.anonymous',
     defaultMessage: `Anonymous users can't vote`,
   },
-  'vote.only_positive': {
-    id: 'vote.only_positive',
+  onlyPositive: {
+    id: 'vote.only-positive',
     defaultMessage: `Only positive score allowed`,
   },
 });
@@ -43,34 +43,13 @@ export enum VoteMessagesTypes {
 
 export function getVoteMessage(type: VoteMessagesTypes, intl: IntlShape) {
   const messages = {
-    [VoteMessagesTypes.OWN_COMMENT]: intl.formatMessage({
-      id: 'vote.own-comment',
-      defaultMessage: 'vote.own-comment',
-    }),
-    [VoteMessagesTypes.GUEST]: intl.formatMessage({
-      id: 'vote.guest',
-      defaultMessage: 'vote.guest',
-    }),
-    [VoteMessagesTypes.ONLY_POST_PAGE]: intl.formatMessage({
-      id: 'vote.only-post-page',
-      defaultMessage: 'vote.only-post-page',
-    }),
-    [VoteMessagesTypes.READONLY]: intl.formatMessage({
-      id: 'vote.readonly',
-      defaultMessage: 'vote.readonly',
-    }),
-    [VoteMessagesTypes.DELETED]: intl.formatMessage({
-      id: 'vote.deleted',
-      defaultMessage: 'vote.deleted',
-    }),
-    [VoteMessagesTypes.ANONYMOUS]: intl.formatMessage({
-      id: 'vote.anonymous',
-      defaultMessage: 'vote.anonymous',
-    }),
-    [VoteMessagesTypes.ONLY_POSITIVE]: intl.formatMessage({
-      id: 'vote.only_positive',
-      defaultMessage: 'vote.only_positive',
-    }),
+    [VoteMessagesTypes.OWN_COMMENT]: intl.formatMessage(voteMessages.ownComment),
+    [VoteMessagesTypes.GUEST]: intl.formatMessage(voteMessages.guest),
+    [VoteMessagesTypes.ONLY_POST_PAGE]: intl.formatMessage(voteMessages.onlyPostPage),
+    [VoteMessagesTypes.READONLY]: intl.formatMessage(voteMessages.readonly),
+    [VoteMessagesTypes.DELETED]: intl.formatMessage(voteMessages.deleted),
+    [VoteMessagesTypes.ANONYMOUS]: intl.formatMessage(voteMessages.anonymous),
+    [VoteMessagesTypes.ONLY_POSITIVE]: intl.formatMessage(voteMessages.onlyPositive),
   };
   return messages[type];
 }
