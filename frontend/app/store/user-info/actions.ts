d1095297a7513b270681b61f2fd64e8311192409
Pavel Mineev <pavel@mineev.me>
2020-03-06 21:29:31 +0300

    Remove unused functions

--- frontend/app/store/user-info/actions.ts
+++ frontend/app/store/user-info/actions.ts
@@ -1,20 +1,9 @@
 import api from '@app/common/api';
-import { Comment, User } from '@app/common/types';
+import { Comment } from '@app/common/types';
 import { userInfo } from '@app/common/user-info-settings';
 
 import { StoreAction } from '../index';
 import { USER_INFO_SET } from './types';
-
-export const getUserComments = (userId: User['id']): StoreAction<Comment[] | null> => (_dispatch, getState) =>
-  getState().userComments![userId] || null;
-
-export const getComments = (id: User['id']): StoreAction<Comment[] | null> => (_dispatch, getState) => {
-  const comments = getState().userComments![id];
-  if (comments) {
-    return comments;
-  }
-  return null;
-};
 
 export const fetchInfo = (): StoreAction<Promise<Comment[] | null>> => async dispatch => {
   if (!userInfo.id) {
