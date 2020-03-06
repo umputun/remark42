e8e6d9a41793a168918a9339c19c59e02a569838
Pavel Mineev <pavel@mineev.me>
2020-03-06 21:30:05 +0300

    Merge in conditions in one

--- frontend/app/store/comments/getters.ts
+++ frontend/app/store/comments/getters.ts
@@ -2,11 +2,9 @@ import { Comment, CommentMode } from '@app/common/types';
 import { StoreState } from '../index';
 
 export const getCommentMode = (id: Comment['id']) => (state: StoreState): CommentMode => {
-  if (state.activeComment === null) {
+  if (state.activeComment === null || state.activeComment.id !== id) {
     return CommentMode.None;
   }
-  if (state.activeComment.id !== id) {
-    return CommentMode.None;
-  }
+
   return state.activeComment.state;
 };
