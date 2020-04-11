1e67850e877b33b97af03b32ff6cfacef0a2aa21
Dmitry Verkhoturov <paskal.07@gmail.com>
2020-04-11 18:35:12 +0200

    fix docker run error without attached /srv/var

--- docker-init.sh
+++ docker-init.sh
@@ -5,4 +5,11 @@ sed -i "s|https://demo.remark42.com|${REMARK_URL}|g" /srv/web/*.js
 # remove devtools attach helper. TODO: move to webpack loader
 sed -i "/REMOVE-START/,/REMOVE-END/d" /srv/web/iframe.html
 
-chown -R app:app /srv/var 2>/dev/null
+if [ -d "/srv/var" ]; then
+  chown -R app:app /srv/var 2>/dev/null
+else
+  echo "ERROR: /srv/var doesn't exist, which means that state of the application"
+  echo "ERROR: will be lost on container stop or restart."
+  echo "ERROR: Please mount local directory to /srv/var in order for it to work."
+  exit 199
+fi
