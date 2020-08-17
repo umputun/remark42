a59be983f248c2d4e3c6e0e685a20fc1c8fcc763
Pavel Pustovalov <pustovalov.pavel@gmail.com>
2020-08-18 02:53:19 +0300

    Update lint-staged & husky

--- frontend/.lintstagedrc.js
+++ frontend/.lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  './**/*.{ts,tsx,js,jsx}': ['eslint --fix --max-warnings=0', 'prettier --write', 'git add'],
-  './**/*.{scss,pcss,css}': ['prettier --write', 'stylelint', 'git add'],
-  './iframe.html': ['prettier --write', 'stylelint', 'git add'],
+  './**/*.{ts,tsx,js,jsx}': ['eslint --fix --max-warnings=0', 'prettier --write'],
+  './**/*.{scss,pcss,css}': ['prettier --write', 'stylelint'],
+  './iframe.html': ['prettier --write', 'stylelint'],
 };
