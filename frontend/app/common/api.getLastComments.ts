d86a9cd7960b52f37926e1bfd23163b1cacb3975
Pavel Mineev <pavel@mineev.me>
2021-01-31 23:51:40 +0300

    fix last comments request

--- frontend/app/common/api.getLastComments.ts
+++ frontend/app/common/api.getLastComments.ts
@@ -2,5 +2,5 @@ import { Comment } from './types';
 import { apiFetcher } from './fetcher';
 
 export default function getLastComments(siteId: string, max: number): Promise<Comment[]> {
-  return apiFetcher.get(`/last/${max}?site=${siteId}`);
+  return apiFetcher.get(`/last/${max}`, { site: siteId });
 }
