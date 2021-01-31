bef3296308f78d0bb402c64da2020a341fbd7791
Pavel Mineev <pavel@mineev.me>
2021-01-31 22:48:44 +0300

    rid of legacy bundles from size-limit

--- frontend/.size-limit.js
+++ frontend/.size-limit.js
@@ -4,16 +4,8 @@ module.exports = [
     limit: '2.5 KB',
   },
   {
-    path: 'public/embed.js',
-    limit: '2.5 KB',
-  },
-  {
     path: 'public/remark.mjs',
     limit: '70 KB',
-  },
-  {
-    path: 'public/remark.js',
-    limit: '73.5 KB',
   },
   {
     path: 'public/remark.css',
@@ -24,10 +16,6 @@ module.exports = [
     limit: '31 KB',
   },
   {
-    path: 'public/last-comments.js',
-    limit: '38 KB',
-  },
-  {
     path: 'public/last-comments.css',
     limit: '6 KB',
   },
@@ -36,15 +24,7 @@ module.exports = [
     limit: '11 KB',
   },
   {
-    path: 'public/deleteme.js',
-    limit: '18 KB',
-  },
-  {
     path: 'public/counter.mjs',
-    limit: '0.7 KB',
-  },
-  {
-    path: 'public/counter.js',
     limit: '0.7 KB',
   },
 ];
