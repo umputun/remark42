6d901e4b11a6690c28a80704e26f7349f74d3bcb
Enes Cakir <enes@cakir.web.tr>
2021-02-11 00:24:51 +0300

    Fix user ID encoding for empty string

--- backend/app/store/user.go
+++ backend/app/store/user.go
@@ -48,9 +48,8 @@ func EncodeID(id string) string {
 
 // hashWithFallback tries to has val with hash.Hash and fallback to crc if needed
 func hashWithFallback(h hash.Hash, val string) string {
-
-	if reValidSha.MatchString(val) {
-		return val // already hashed or empty
+	if len(val) == 0 || reValidSha.MatchString(val) {
+		return val // empty or already hashed
 	}
 
 	if _, err := io.WriteString(h, val); err != nil {
