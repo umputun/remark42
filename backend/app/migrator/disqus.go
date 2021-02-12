1151413d3139eb7d3d2dafed07736e6951152bce
Enes Cakir <enes@cakir.web.tr>
2021-02-12 23:28:16 +0300

    Fix linter emptyStringTest error

--- backend/app/migrator/disqus.go
+++ backend/app/migrator/disqus.go
@@ -137,7 +137,7 @@ func (d *Disqus) convert(r io.Reader, siteID string) (ch chan store.Comment) {
 						ParentID:  comment.Pid.Val,
 						Imported:  true,
 					}
-					if len(comment.AuthorUserName) == 0 { // empty comment.AuthorUserName from disqus
+					if comment.AuthorUserName == "" { // empty comment.AuthorUserName from disqus
 						c.User.ID = "disqus_" + c.User.Name
 					}
 					if c.ID == "" { // no comment.UID
