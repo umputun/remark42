54679a29ff2ab59f8969107cf09a66f375166616
Elephmoon <alexandr.kizilow@gmail.com>
2021-03-04 07:59:36 +0300

    postMessage without serialization

--- frontend/app/common/user-info-settings.ts
+++ frontend/app/common/user-info-settings.ts
@@ -4,8 +4,6 @@ import type { UserInfo } from './types';
 
 export const userInfo: UserInfo = parseQuery();
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const isDefaultPicture = ((userInfo.isDefaultPicture as any) as string) !== '1';
 export const id = userInfo.id;
 export const name = userInfo.name;
 export const picture = userInfo.picture;
--- frontend/app/utils/postMessage.ts
+++ frontend/app/utils/postMessage.ts
@@ -1,22 +1,42 @@
-import { User } from 'common/types';
+import type { Theme, UserInfo } from 'common/types';
 
-export type Message =
-  | { inited: true }
-  | {
-      isUserInfoShown: true;
-      user: User;
-    }
-  | { isUserInfoShown: false }
-  | { scrollTo: number }
-  | { remarkIframeHeight: number };
+export type ParentMessage = {
+  inited?: true;
+  scrollTo?: number;
+  remarkIframeHeight?: number;
+} & (
+  | { isUserInfoShown: true; user: UserInfo }
+  | { isUserInfoShown: false; user?: never }
+  | { isUserInfoShown?: never; user?: never }
+);
+
+export type ChildMessage = {
+  theme?: Theme;
+};
+
+type AllMessages = ParentMessage & ChildMessage;
 
 /**
  * Sends message to parent window
  *
  * @returns request success of fail
  */
-export function postMessage(data: Message): boolean {
+export function postMessage(data: AllMessages): boolean {
   if (!window.parent || window.parent === window) return false;
-  window.parent.postMessage(JSON.stringify(data), '*');
+  window.parent.postMessage(data, '*');
   return true;
+}
+
+/**
+ * Parses data from post message that was received in iframe
+ *
+ * @param evt post message event
+ * @returns
+ */
+export function parseMessage<T>({ data }: MessageEvent<T>): T {
+  if (typeof data !== 'object' || data === null || Array.isArray(data)) {
+    return {} as T;
+  }
+
+  return data as T;
 }
