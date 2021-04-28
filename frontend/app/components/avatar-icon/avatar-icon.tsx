3c72eb7d66fe80dbcd4a590a3d2093862b60a330
Pavel Mineev <pavel@mineev.me>
2021-04-28 22:05:10 +0300

    fix default avatar
    - fix import of default avatar
    - optimaze ghost svg
    - add tests
    - refactor avatar-icon

--- frontend/app/components/avatar-icon/avatar-icon.tsx
+++ frontend/app/components/avatar-icon/avatar-icon.tsx
@@ -2,18 +2,19 @@ import { h, JSX } from 'preact';
 import b from 'bem-react-helper';
 import { Theme } from 'common/types';
 
-interface Props {
+type Props = JSX.HTMLAttributes<HTMLImageElement> & {
   picture?: string;
   mix?: string;
   theme?: Theme;
-}
+};
 
-export function AvatarIcon(props: Props & JSX.HTMLAttributes) {
+export function AvatarIcon({ mix, theme, picture, ...props }: Props) {
   return (
     <img
-      className={b('avatar-icon', { mix: props.mix }, { theme: props.theme, default: !props.picture })}
-      src={props.picture || require('./avatar-icon.svg')}
+      className={b('avatar-icon', { mix }, { theme, default: !picture })}
+      src={picture || require('./avatar-icon.svg').default}
       alt=""
+      {...props}
     />
   );
 }
