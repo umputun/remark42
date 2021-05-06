fe6963061aad0e51e90dec492ec7e8d969c04fb4
Umputun <umputun@gmail.com>
2021-05-06 16:53:25 -0500

    lint: remove commented test code

--- backend/app/migrator/migrator_test.go
+++ backend/app/migrator/migrator_test.go
@@ -20,12 +20,6 @@ func TestMigrator_ImportDisqus(t *testing.T) {
 	defer func() {
 		os.Remove("/tmp/remark-test.db")
 	}()
-	//
-	// fh, err := os.Open("testdata/disqus1.xml")
-	// require.NoError(t, err)
-	//
-	// err := ioutil.WriteFile("/tmp/disqus-test.xml", []byte(xmlTestDisqus), 0600)
-	// require.NoError(t, err)
 
 	b, err := engine.NewBoltDB(bolt.Options{}, engine.BoltSite{FileName: "/tmp/remark-test.db", SiteID: "test"})
 	require.NoError(t, err, "create store")
