82387729a4da0b8316514bdf91b9cd477524f2ff
Dmitry Verkhoturov <paskal.07@gmail.com>
2021-05-24 12:21:53 +0200

    make docker init script chown command verbose

--- docker-init.sh
+++ docker-init.sh
@@ -9,7 +9,8 @@ if [ -n "${SITE_ID}" ]; then
 fi
 
 if [ -d "/srv/var" ]; then
-  chown -R app:app /srv/var 2>/dev/null
+  echo "changing ownership of /srv/var to app:app (remark42 user inside the container)"
+  chown -R app:app /srv/var || echo "WARNING: /srv/var ownership change failed, if application will fail that might be the reason"
 else
   echo "ERROR: /srv/var doesn't exist, which means that state of the application"
   echo "ERROR: will be lost on container stop or restart."
