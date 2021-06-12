91be16c9a63c9849d54188621bfbc73bc73eca4c
Pavel Mineev <pavel@mineev.me>
2021-06-12 14:47:50 +0300

    cache github data from first load

--- site/src/data/github.js
+++ site/src/data/github.js
@@ -1,10 +1,26 @@
 const fetch = require('node-fetch')
 
+const DEFAULT_DATA = { latestVersion: '' }
+let currentData = null
+
 module.exports = async function getLatestReleaseVersion() {
+	if (currentData) {
+		return currentData
+	}
+
 	const res = await fetch(
 		'https://api.github.com/repos/umputun/remark42/releases'
 	)
+
+	if (!res.ok) {
+		console.error(`[ERROR] Status: ${res.status}: ${res.statusText}`)
+
+		return DEFAULT_DATA
+	}
+
 	const data = await res.json()
 
-	return { latestVersion: data[0].tag_name }
+	currentData = { latestVersion: data[0].tag_name || '' }
+
+	return currentData
 }
