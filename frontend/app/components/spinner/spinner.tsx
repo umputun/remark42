020b11d25643b685d2c19a290a5de78bfa8f3118
Pavel Mineev <pavel@mineev.me>
2021-08-06 09:58:29 +0300

    fix typo

--- frontend/app/components/spinner/spinner.tsx
+++ frontend/app/components/spinner/spinner.tsx
@@ -3,7 +3,7 @@ import { h } from 'preact';
 import { messages } from 'components/auth/auth.messsages';
 import { useIntl } from 'react-intl';
 
-import styles from './Spinner.module.css';
+import styles from './spinner.module.css';
 
 export function Spinner() {
   const intl = useIntl();
