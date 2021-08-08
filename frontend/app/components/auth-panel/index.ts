cac1eb2601364b19ad470c7b403e9c46a7138e99
Pavel Mineev <pavel@mineev.me>
2021-08-08 17:18:02 +0300

    remove unused code

--- frontend/app/components/auth-panel/index.ts
+++ frontend/app/components/auth-panel/index.ts
@@ -2,6 +2,5 @@ import './auth-panel.css';
 
 import './__readonly-label/auth-panel__readonly-label.css';
 import './__column/auth-panel__column.css';
-import './__sort/auth-panel__sort.css';
 
 export * from './auth-panel';
