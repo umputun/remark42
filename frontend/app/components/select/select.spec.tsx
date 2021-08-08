ea9d164aff1b9c07996607f55cafc5f30f4657ca
Pavel Mineev <pavel@mineev.me>
2021-08-08 16:47:55 +0300

    add tests for sort picker and update select tests

--- frontend/app/components/select/select.spec.tsx
+++ frontend/app/components/select/select.spec.tsx
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom';
+import { fireEvent, waitFor } from '@testing-library/preact';
 import { h } from 'preact';
 import { render } from 'tests/utils';
 import { Select } from './select';
@@ -29,18 +30,31 @@ const items = [
 describe('<Select/>', () => {
   it('should has static class names', () => {
     const { container } = render(<Select items={items} selected={items[0]} />);
+    const selectElement = container.querySelector('.select-element');
 
     expect(container.querySelector('.select')).toBeInTheDocument();
     expect(container.querySelector('.select-arrow')).toBeInTheDocument();
-    expect(container.querySelector('.select-element')).toBeInTheDocument();
+    expect(selectElement).toBeInTheDocument();
+    fireEvent.focus(selectElement as HTMLSelectElement);
+    expect(container.querySelector('.select_focused')).toBeInTheDocument();
   });
 
   it('should render selected item', () => {
     const { container, getAllByText } = render(<Select items={items} selected={items[0]} />);
-    const selectedOption = container.querySelectorAll('option')[0];
+    const selectedOption = container.querySelector('option');
 
     expect(getAllByText(items[0].label)).toHaveLength(2);
-    expect(selectedOption.selected).toBeTruthy();
-    expect(selectedOption.textContent).toBe(items[0].label);
+    expect(selectedOption).toBeInTheDocument();
+    expect(selectedOption?.selected).toBeTruthy();
+    expect(selectedOption?.textContent).toBe(items[0].label);
+  });
+
+  it('should highlight selet on focus', async () => {
+    const { container } = render(<Select items={items} selected={items[0]} />);
+    const select = container.querySelector('select');
+
+    expect(container.querySelector('select')).toBeInTheDocument();
+    fireEvent.focus(select as HTMLSelectElement);
+    expect(container.querySelector('.rootFocused')).toBeInTheDocument();
   });
 });
