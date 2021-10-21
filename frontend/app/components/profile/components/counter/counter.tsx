64846e3ff91c4a70eabb4a0bd17336462905275d
esvyridov <eugene.svyridov@gmail.com>
2021-10-21 23:58:25 +0200

    Add tests for comments counter in profile sidebar

--- frontend/app/components/profile/components/counter/counter.tsx
+++ frontend/app/components/profile/components/counter/counter.tsx
@@ -2,5 +2,9 @@ import { h } from 'preact';
 import styles from './counter.module.css';
 
 export const Counter: React.FC = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container} data-testid="comments-counter">
+      {children}
+    </div>
+  );
 };
