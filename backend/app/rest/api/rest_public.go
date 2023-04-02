vdimir/remark

Full text search

backend/app/rest/api/rest_public.go
@@ -42,6 +42,7 @@ type pubStore interface {
 	Count(locator store.Locator) (int, error)
 	List(siteID string, limit, skip int) ([]store.PostInfo, error)
 	Info(locator store.Locator, readonlyAge int) (store.PostInfo, error)
+	Search(siteID, query, sortBy string, limit, skip int) ([]store.Comment, uint64, error)
 
 	ValidateComment(c *store.Comment) error
 	IsReadOnly(locator store.Locator) bool
@@ -383,6 +384,73 @@ func (s *public) telegramQrCtrl(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/png")
 	if _, err = w.Write(png); err != nil {
 		log.Printf("[WARN] can't render qr, %v", err)
+	}
+}
+
+// GET /search?site=siteID&query=query&limit=20&skip=10&sort=[-]field - search documents
+// site - site ID
+// query - user-provided search query
+// limit - number of documents to return
+// skip - number of documents to skip
+// sort - sort by specified field, can be prefixed with "-" to reverse the order
+func (s *public) searchQueryCtrl(w http.ResponseWriter, r *http.Request) {
+	getIntParamInRange := func(min, max, defaultVal int, name string) (int, error) {
+		s := r.URL.Query().Get(name)
+		if s == "" {
+			return defaultVal, nil
+		}
+
+		value, err := strconv.Atoi(s)
+		if err != nil {
+			return 0, fmt.Errorf("%s parameter should be an integer", name)
+		}
+
+		if min <= value && value <= max {
+			return value, nil
+		}
+		return 0, fmt.Errorf("%s parameter should be between %d and %d", name, min, max)
+	}
+
+	var err error
+
+	var limit int
+	if limit, err = getIntParamInRange(1, 100, 20, "limit"); err != nil {
+		rest.SendErrorJSON(w, r, http.StatusBadRequest, fmt.Errorf("wrong param"), err.Error(), rest.ErrInternal)
+		return
+	}
+
+	var skip int
+	if skip, err = getIntParamInRange(0, 1000, 0, "skip"); err != nil {
+		rest.SendErrorJSON(w, r, http.StatusBadRequest, fmt.Errorf("wrong param"), err.Error(), rest.ErrInternal)
+		return
+	}
+
+	siteID := r.URL.Query().Get("site")
+	query := r.URL.Query().Get("query")
+	sortBy := r.URL.Query().Get("sort")
+
+	if siteID == "" || query == "" {
+		rest.SendErrorJSON(w, r, http.StatusBadRequest, fmt.Errorf("missing param"), "site and query parameters are required", rest.ErrInternal)
+		return
+	}
+
+	key := cache.NewKey(siteID).ID(URLKey(r)).Scopes(siteID, searchScope)
+	data, err := s.cache.Get(key, func() ([]byte, error) {
+		comments, total, searchErr := s.dataService.Search(siteID, query, sortBy, limit, skip)
+		if searchErr != nil {
+			return nil, searchErr
+		}
+
+		return encodeJSONWithHTML(commentsWithTotal{Comments: comments, Total: total})
+	})
+
+	if err != nil {
+		rest.SendErrorJSON(w, r, http.StatusInternalServerError, err, "can't perform search request", rest.ErrInternal)
+		return
+	}
+
+	if err = R.RenderJSONFromBytes(w, r, data); err != nil {
+		log.Printf("[WARN] can't render search results for site %s", siteID)
 	}
 }
 

backend/app/rest/api/rest_test.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptest"
+	urlpkg "net/url"
 	"os"
 	"strconv"
 	"strings"
@@ -34,6 +35,7 @@ import (
 	adminstore "github.com/umputun/remark42/backend/app/store/admin"
 	"github.com/umputun/remark42/backend/app/store/engine"
 	"github.com/umputun/remark42/backend/app/store/image"
+	"github.com/umputun/remark42/backend/app/store/search"
 	"github.com/umputun/remark42/backend/app/store/service"
 )
 
@@ -205,6 +207,87 @@ func TestRest_rejectAnonUser(t *testing.T) {
 	require.NoError(t, err)
 	require.NoError(t, resp.Body.Close())
 	assert.Equal(t, http.StatusOK, resp.StatusCode, "real user")
+}
+
+func TestRest_Search(t *testing.T) {
+	var searchIndexPath string
+	ts, _, teardown := startupT(t, func(srv *Rest) {
+		var err error
+		searchIndexPath, err = randomPath(srv.WebRoot, "test-search-remark", "")
+		require.NoError(t, err)
+		srv.DataService.SearchService, err = search.NewService([]string{"remark42"}, search.ServiceParams{
+			IndexPath: searchIndexPath,
+			Analyzer:  "standard",
+		})
+		require.NoError(t, err)
+	})
+	defer teardown()
+	defer func() { _ = os.RemoveAll(searchIndexPath) }()
+
+	t0 := time.Date(2018, 12, 20, 15, 18, 22, 0, time.Local)
+
+	id1 := addComment(t, store.Comment{Text: "test test", Timestamp: t0.Add(1 * time.Minute), Locator: store.Locator{SiteID: "remark42", URL: "https://radio-t.com/abc"}}, ts)
+	id2 := addComment(t, store.Comment{Text: "test hello", Timestamp: t0.Add(2 * time.Minute), Locator: store.Locator{SiteID: "remark42", URL: "https://radio-t.com/blah"}}, ts)
+	id3 := addComment(t, store.Comment{Text: "hello world", Timestamp: t0.Add(3 * time.Minute), Locator: store.Locator{SiteID: "remark42", URL: "https://radio-t.com/blah"}}, ts)
+	id4 := addComment(t, store.Comment{Text: "# title\n\nok\n", Timestamp: t0.Add(4 * time.Minute), Locator: store.Locator{SiteID: "remark42", URL: "https://radio-t.com/blah"}}, ts)
+
+	idToPos := map[string]int{id1: 1, id2: 2, id3: 3, id4: 4}
+	tbl := []struct {
+		query       string
+		extraParams string
+		ids         []int
+		status      int
+	}{
+		{"blah", "", []int{}, http.StatusOK},
+		{"h1", "", []int{}, http.StatusOK},
+		{"world", "", []int{3}, http.StatusOK},
+		{"title", "", []int{4}, http.StatusOK},
+		{"\"hello world\"", "", []int{3}, http.StatusOK},
+		{"test", "&sort=time", []int{1, 2}, http.StatusOK},
+		{"hello", "&sort=-time", []int{3, 2}, http.StatusOK},
+		{"hello world", "&sort=time", []int{2, 3}, http.StatusOK},
+		{"hello world test", "&sort=time", []int{1, 2, 3}, http.StatusOK},
+		{"hello world test", "&sort=time&skip=1", []int{2, 3}, http.StatusOK},
+		{"hello world test", "&sort=time&skip=1&limit=1", []int{2}, http.StatusOK},
+		{"", "", []int{}, http.StatusBadRequest},
+		{"test", "&skip=-1", []int{}, http.StatusBadRequest},
+		{"test", "&limit=999999", []int{}, http.StatusBadRequest},
+
+		// this field is checked inside search service,
+		// and the handler doesn't know the rules for the validation of the field
+		{"test", "&sort=text", []int{}, http.StatusInternalServerError},
+	}
+
+	cnt := 0
+	for i, tt := range tbl {
+		tt := tt
+
+		cnt += 1
+		if (cnt % 10) == 0 {
+			// wait for rate limiter
+			time.Sleep(1 * time.Second)
+		}
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			resp, err := http.Get(fmt.Sprintf("%s/api/v1/search?site=remark42&query=%s%s",
+				ts.URL, urlpkg.QueryEscape(tt.query), tt.extraParams))
+			require.NoError(t, err)
+
+			defer resp.Body.Close()
+			body, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			assert.Equal(t, tt.status, resp.StatusCode)
+
+			result := commentsWithTotal{}
+			err = json.Unmarshal(body, &result)
+			require.NoError(t, err)
+
+			foundIds := make([]int, len(result.Comments))
+			for i, c := range result.Comments {
+				foundIds[i] = idToPos[c.ID]
+			}
+			require.Equal(t, tt.ids, foundIds)
+		})
+	}
 }
 
 func Test_URLKey(t *testing.T) {
@@ -628,5 +711,9 @@ func waitForHTTPSServerStart(port int) {
 }
 
 func TestMain(m *testing.M) {
-	goleak.VerifyTestMain(m)
+	goleak.VerifyTestMain(m,
+		// we should call bleve.Config.Shutdown() to close all the workers,
+		// but we don't do it for each server instance because it's global per application
+		goleak.IgnoreTopFunction("github.com/blevesearch/bleve_index_api.AnalysisWorker"),
+	)
 }

backend/app/store/service/service.go
@@ -3,6 +3,7 @@
 package service
 
 import (
+	"context"
 	"fmt"
 	"math"
 	"sort"
@@ -12,6 +13,7 @@ import (
 
 	"github.com/go-pkgz/lcw"
 	log "github.com/go-pkgz/lgr"
+	"github.com/go-pkgz/syncs"
 	"github.com/google/uuid"
 	"github.com/hashicorp/go-multierror"
 
@@ -19,6 +21,7 @@ import (
 	"github.com/umputun/remark42/backend/app/store/admin"
 	"github.com/umputun/remark42/backend/app/store/engine"
 	"github.com/umputun/remark42/backend/app/store/image"
+	"github.com/umputun/remark42/backend/app/store/search"
 )
 
 // DataStore wraps store.Interface with additional methods
@@ -36,6 +39,7 @@ type DataStore struct {
 	TitleExtractor         *TitleExtractor
 	RestrictedWordsMatcher *RestrictedWordsMatcher
 	ImageService           *image.Service
+	SearchService          *search.Service
 	AdminEdits             bool // allow admin unlimited edits
 
 	// granular locks
@@ -104,6 +108,10 @@ func (s *DataStore) Create(comment store.Comment) (commentID string, err error) 
 	commentID, err = s.Engine.Create(comment)
 	s.submitImages(comment)
 
+	if e := s.indexComment(comment); e != nil {
+		log.Printf("[WARN] failed to index comment, %v", e)
+	}
+
 	if e := s.AdminStore.OnEvent(comment.Locator.SiteID, admin.EvCreate); e != nil {
 		log.Printf("[WARN] failed to send create event, %s", e)
 	}
@@ -152,12 +160,6 @@ func (s *DataStore) Get(locator store.Locator, commentID string, user store.User
 		return store.Comment{}, err
 	}
 	return s.alterComment(c, user), nil
-}
-
-// Put updates comment, mutable parts only
-func (s *DataStore) Put(locator store.Locator, comment store.Comment) error {
-	comment.Locator = locator
-	return s.Engine.Update(comment)
 }
 
 // GetUserEmail gets user email
@@ -522,6 +524,11 @@ func (s *DataStore) EditComment(locator store.Locator, commentID string, req Edi
 	}
 
 	err = s.Engine.Update(comment)
+
+	if e := s.indexComment(comment); e != nil {
+		log.Printf("[WARN] failed to update comment index, %v", e)
+	}
+
 	return comment, err
 }
 
@@ -907,6 +914,49 @@ func (s *DataStore) Last(siteID string, limit int, since time.Time, user store.U
 	return s.alterComments(comments, user), nil
 }
 
+// Search executes search query and enriches the result with comment content
+func (s *DataStore) Search(siteID, query, sortBy string, limit, skip int) ([]store.Comment, uint64, error) {
+	if s.SearchService == nil {
+		return []store.Comment{}, 0, fmt.Errorf("search service is not enabled")
+	}
+	req := &search.Request{
+		SiteID: siteID,
+		Query:  query,
+		Limit:  limit,
+		Offset: skip,
+		SortBy: sortBy,
+	}
+
+	seqrchRes, err := s.SearchService.Search(req)
+	if err != nil {
+		return nil, 0, fmt.Errorf("search failed: %w", err)
+	}
+
+	comments := make([]store.Comment, 0, len(seqrchRes.Keys))
+	for _, docKey := range seqrchRes.Keys {
+		comment, err := s.Get(docKey.Locator, docKey.ID, nonAdminUser)
+		if err != nil {
+			// ignore error because comment can be deleted from the store (but we still have it in search index)
+			continue
+		}
+		comments = append(comments, comment)
+	}
+	return comments, seqrchRes.Total, nil
+}
+
+// RunSiteIndexers indexes all comments for siteID in the sized group.
+// It's required to call this method at the startup to support "cold start"
+// when store contains some data, but search functionality is enabled for the first time.
+func (s *DataStore) RunSiteIndexers(ctx context.Context, siteID string, grp *syncs.ErrSizedGroup) {
+	if s.SearchService == nil {
+		return
+	}
+	err := search.IndexSite(ctx, siteID, s.SearchService, s.Engine, grp)
+	if err != nil {
+		log.Printf("[WARN] error occurred during indexing comments for site %q: %e", siteID, err)
+	}
+}
+
 // Close store service
 func (s *DataStore) Close() error {
 	errs := new(multierror.Error)
@@ -915,6 +965,9 @@ func (s *DataStore) Close() error {
 	}
 	if s.TitleExtractor != nil {
 		errs = multierror.Append(errs, s.TitleExtractor.Close())
+	}
+	if s.SearchService != nil {
+		errs = multierror.Append(errs, s.SearchService.Close())
 	}
 	errs = multierror.Append(errs, s.Engine.Close())
 	return errs.ErrorOrNil()
@@ -1012,4 +1065,14 @@ func (s *DataStore) getSecret(siteID string) (secret string, err error) {
 		return "", fmt.Errorf("site %s disabled", siteID)
 	}
 	return secret, nil
+}
+
+func (s *DataStore) indexComment(comment store.Comment) error {
+	if s.SearchService != nil {
+		err := s.SearchService.Index(comment)
+		if err != nil {
+			return fmt.Errorf("can't index comment %s: %w", comment.ID, err)
+		}
+	}
+	return nil
 }

umputun/remark42

backend/app/rest/api/rest_public.go
@@ -42,6 +42,7 @@ type pubStore interface {
 	Count(locator store.Locator) (int, error)
 	List(siteID string, limit, skip int) ([]store.PostInfo, error)
 	Info(locator store.Locator, readonlyAge int) (store.PostInfo, error)
+	Search(siteID, query, sortBy string, limit, skip int) ([]store.Comment, uint64, error)
 
 	ValidateComment(c *store.Comment) error
 	IsReadOnly(locator store.Locator) bool
@@ -383,6 +384,73 @@ func (s *public) telegramQrCtrl(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "image/png")
 	if _, err = w.Write(png); err != nil {
 		log.Printf("[WARN] can't render qr, %v", err)
+	}
+}
+
+// GET /search?site=siteID&query=query&limit=20&skip=10&sort=[-]field - search documents
+// site - site ID
+// query - user-provided search query
+// limit - number of documents to return, default is 20, maximum is 100
+// skip - number of documents to skip, default is 0 (do not skip anything), maximum is 1000
+// sort - sort by specified field, can be prefixed with "-" to reverse the order
+func (s *public) searchQueryCtrl(w http.ResponseWriter, r *http.Request) {
+	getIntParamInRange := func(min, max, defaultVal int, name string) (int, error) {
+		s := r.URL.Query().Get(name)
+		if s == "" {
+			return defaultVal, nil
+		}
+
+		value, err := strconv.Atoi(s)
+		if err != nil {
+			return 0, fmt.Errorf("%s parameter should be an integer", name)
+		}
+
+		if min <= value && value <= max {
+			return value, nil
+		}
+		return 0, fmt.Errorf("%s parameter should be between %d and %d", name, min, max)
+	}
+
+	var err error
+
+	var limit int
+	if limit, err = getIntParamInRange(1, 100, 20, "limit"); err != nil {
+		rest.SendErrorJSON(w, r, http.StatusBadRequest, fmt.Errorf("wrong param"), err.Error(), rest.ErrInternal)
+		return
+	}
+
+	var skip int
+	if skip, err = getIntParamInRange(0, 1000, 0, "skip"); err != nil {
+		rest.SendErrorJSON(w, r, http.StatusBadRequest, fmt.Errorf("wrong param"), err.Error(), rest.ErrInternal)
+		return
+	}
+
+	siteID := r.URL.Query().Get("site")
+	query := r.URL.Query().Get("query")
+	sortBy := r.URL.Query().Get("sort")
+
+	if siteID == "" || query == "" {
+		rest.SendErrorJSON(w, r, http.StatusBadRequest, fmt.Errorf("missing param"), "site and query parameters are required", rest.ErrInternal)
+		return
+	}
+
+	key := cache.NewKey(siteID).ID(URLKey(r)).Scopes(siteID, searchScope)
+	data, err := s.cache.Get(key, func() ([]byte, error) {
+		comments, total, searchErr := s.dataService.Search(siteID, query, sortBy, limit, skip)
+		if searchErr != nil {
+			return nil, searchErr
+		}
+
+		return encodeJSONWithHTML(commentsWithTotal{Comments: comments, Total: total})
+	})
+
+	if err != nil {
+		rest.SendErrorJSON(w, r, http.StatusBadRequest, err, "can't perform search request", rest.ErrInternal)
+		return
+	}
+
+	if err = R.RenderJSONFromBytes(w, r, data); err != nil {
+		log.Printf("[WARN] can't render search results for site %s", siteID)
 	}
 }
 

backend/app/rest/api/rest_test.go
@@ -10,6 +10,7 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptest"
+	urlpkg "net/url"
 	"os"
 	"strconv"
 	"strings"
@@ -34,6 +35,7 @@ import (
 	adminstore "github.com/umputun/remark42/backend/app/store/admin"
 	"github.com/umputun/remark42/backend/app/store/engine"
 	"github.com/umputun/remark42/backend/app/store/image"
+	"github.com/umputun/remark42/backend/app/store/search"
 	"github.com/umputun/remark42/backend/app/store/service"
 )
 
@@ -210,6 +212,94 @@ func TestRest_rejectAnonUser(t *testing.T) {
 	require.NoError(t, err)
 	require.NoError(t, resp.Body.Close())
 	assert.Equal(t, http.StatusOK, resp.StatusCode, "real user")
+}
+
+func TestRest_Search(t *testing.T) {
+	var searchIndexPath string
+	ts, _, teardown := startupT(t, func(srv *Rest) {
+		var err error
+		searchIndexPath, err = randomPath(srv.WebRoot, "test-search-remark", "")
+		require.NoError(t, err)
+		srv.DataService.SearchService, err = search.NewService([]string{"remark42"}, search.ServiceParams{
+			IndexPath: searchIndexPath,
+			Analyzer:  "standard",
+		})
+		require.NoError(t, err)
+	})
+	defer teardown()
+	defer func() { _ = os.RemoveAll(searchIndexPath) }()
+
+	t0 := time.Date(2018, 12, 20, 15, 18, 22, 0, time.Local)
+
+	id1 := addComment(t, store.Comment{Text: "test test", Timestamp: t0.Add(1 * time.Minute), Locator: store.Locator{SiteID: "remark42", URL: "https://radio-t.com/abc"}}, ts)
+	id2 := addComment(t, store.Comment{Text: "test hello", Timestamp: t0.Add(2 * time.Minute), Locator: store.Locator{SiteID: "remark42", URL: "https://radio-t.com/blah"}}, ts)
+	id3 := addComment(t, store.Comment{Text: "hello world", Timestamp: t0.Add(3 * time.Minute), Locator: store.Locator{SiteID: "remark42", URL: "https://radio-t.com/blah"}}, ts)
+	id4 := addComment(t, store.Comment{Text: "# title\n\nok\n", Timestamp: t0.Add(4 * time.Minute), Locator: store.Locator{SiteID: "remark42", URL: "https://radio-t.com/blah"}}, ts)
+
+	idToPos := map[string]int{id1: 1, id2: 2, id3: 3, id4: 4}
+	tbl := []struct {
+		query       string
+		extraParams string
+		ids         []int
+		status      int
+	}{
+		{"blah", "", []int{}, http.StatusOK},
+		{"h1", "", []int{}, http.StatusOK},
+		{"world", "", []int{3}, http.StatusOK},
+		{"title", "", []int{4}, http.StatusOK},
+		{"\"hello world\"", "", []int{3}, http.StatusOK},
+		{"test", "&sort=time", []int{1, 2}, http.StatusOK},
+		{"hello", "&sort=-time", []int{3, 2}, http.StatusOK},
+		{"hello world", "&sort=time", []int{2, 3}, http.StatusOK},
+		{"hello world test", "&sort=time", []int{1, 2, 3}, http.StatusOK},
+		{"hello world test", "&sort=time&skip=1", []int{2, 3}, http.StatusOK},
+		{"hello world test", "&sort=time&skip=1&limit=1", []int{2}, http.StatusOK},
+		{"", "", []int{}, http.StatusBadRequest},
+		{"test", "&sort=text", []int{}, http.StatusBadRequest},
+		{"test", "&skip=-1", []int{}, http.StatusBadRequest},
+		{"test", "&limit=999999", []int{}, http.StatusBadRequest},
+	}
+
+	cnt := 0
+	for i, tt := range tbl {
+		tt := tt
+
+		cnt++
+		if (cnt % 10) == 0 {
+			// wait for rate limiter
+			time.Sleep(1 * time.Second)
+		}
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			resp, err := http.Get(fmt.Sprintf("%s/api/v1/search?site=remark42&query=%s%s",
+				ts.URL, urlpkg.QueryEscape(tt.query), tt.extraParams))
+			require.NoError(t, err)
+
+			defer resp.Body.Close()
+			body, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			assert.Equal(t, tt.status, resp.StatusCode)
+
+			result := commentsWithTotal{}
+			err = json.Unmarshal(body, &result)
+			require.NoError(t, err)
+
+			foundIds := make([]int, len(result.Comments))
+			for i, c := range result.Comments {
+				foundIds[i] = idToPos[c.ID]
+			}
+			require.Equal(t, tt.ids, foundIds)
+		})
+	}
+}
+
+func TestRest_SearchDisabledFeature(t *testing.T) {
+	ts, _, teardown := startupT(t)
+	defer teardown()
+
+	resp, err := http.Get(ts.URL + "/api/v1/search?site=remark42&query=test")
+	require.NoError(t, err)
+	require.NoError(t, resp.Body.Close())
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
 }
 
 func Test_URLKey(t *testing.T) {
@@ -633,5 +723,9 @@ func waitForHTTPSServerStart(port int) {
 }
 
 func TestMain(m *testing.M) {
-	goleak.VerifyTestMain(m)
+	goleak.VerifyTestMain(m,
+		// we should call bleve.Config.Shutdown() to close all the workers,
+		// but we don't do it for each server instance because it's global per application
+		goleak.IgnoreTopFunction("github.com/blevesearch/bleve_index_api.AnalysisWorker"),
+	)
 }

backend/app/store/service/service.go
@@ -3,6 +3,7 @@
 package service
 
 import (
+	"context"
 	"fmt"
 	"math"
 	"sort"
@@ -12,6 +13,7 @@ import (
 
 	"github.com/go-pkgz/lcw"
 	log "github.com/go-pkgz/lgr"
+	"github.com/go-pkgz/syncs"
 	"github.com/google/uuid"
 	"github.com/hashicorp/go-multierror"
 
@@ -19,6 +21,7 @@ import (
 	"github.com/umputun/remark42/backend/app/store/admin"
 	"github.com/umputun/remark42/backend/app/store/engine"
 	"github.com/umputun/remark42/backend/app/store/image"
+	"github.com/umputun/remark42/backend/app/store/search"
 )
 
 // DataStore wraps store.Interface with additional methods
@@ -36,6 +39,7 @@ type DataStore struct {
 	TitleExtractor         *TitleExtractor
 	RestrictedWordsMatcher *RestrictedWordsMatcher
 	ImageService           *image.Service
+	SearchService          *search.Service
 	AdminEdits             bool // allow admin unlimited edits
 
 	// granular locks
@@ -103,6 +107,10 @@ func (s *DataStore) Create(comment store.Comment) (commentID string, err error) 
 
 	commentID, err = s.Engine.Create(comment)
 	s.submitImages(comment)
+
+	if e := s.indexCommentForSearch(comment); e != nil {
+		log.Printf("[WARN] failed to index comment for search, %v", e)
+	}
 
 	if e := s.AdminStore.OnEvent(comment.Locator.SiteID, admin.EvCreate); e != nil {
 		log.Printf("[WARN] failed to send create event, %s", e)
@@ -522,6 +530,11 @@ func (s *DataStore) EditComment(locator store.Locator, commentID string, req Edi
 	}
 
 	err = s.Engine.Update(comment)
+
+	if e := s.indexCommentForSearch(comment); e != nil {
+		log.Printf("[WARN] failed to update comment index, %v", e)
+	}
+
 	return comment, err
 }
 
@@ -907,6 +920,49 @@ func (s *DataStore) Last(siteID string, limit int, since time.Time, user store.U
 	return s.alterComments(comments, user), nil
 }
 
+// Search executes search query and enriches the result with comment content
+func (s *DataStore) Search(siteID, query, sortBy string, limit, skip int) ([]store.Comment, uint64, error) {
+	if s.SearchService == nil {
+		return []store.Comment{}, 0, fmt.Errorf("search service is not enabled")
+	}
+	req := &search.Request{
+		SiteID: siteID,
+		Query:  query,
+		Limit:  limit,
+		Skip:   skip,
+		SortBy: sortBy,
+	}
+
+	searchRes, err := s.SearchService.Search(req)
+	if err != nil {
+		return nil, 0, fmt.Errorf("search failed: %w", err)
+	}
+
+	comments := make([]store.Comment, 0, len(searchRes.Keys))
+	for _, docKey := range searchRes.Keys {
+		comment, e := s.Get(docKey.Locator, docKey.ID, nonAdminUser)
+		if e != nil {
+			// ignore error because comment can be deleted from the store (but we still have it in search index)
+			continue
+		}
+		comments = append(comments, comment)
+	}
+	return comments, searchRes.Total, nil
+}
+
+// RunSiteIndexers indexes all comments for siteID in the sized group.
+// It's required to call this method at the startup to support "cold start"
+// when store contains some data, but search functionality is enabled for the first time.
+func (s *DataStore) RunSiteIndexers(ctx context.Context, siteID string, grp *syncs.ErrSizedGroup) {
+	if s.SearchService == nil {
+		return
+	}
+	err := search.IndexSite(ctx, siteID, s.SearchService, s.Engine, grp)
+	if err != nil {
+		log.Printf("[WARN] error occurred during indexing comments for site %q: %e", siteID, err)
+	}
+}
+
 // Close store service
 func (s *DataStore) Close() error {
 	errs := new(multierror.Error)
@@ -915,6 +971,9 @@ func (s *DataStore) Close() error {
 	}
 	if s.TitleExtractor != nil {
 		errs = multierror.Append(errs, s.TitleExtractor.Close())
+	}
+	if s.SearchService != nil {
+		errs = multierror.Append(errs, s.SearchService.Close())
 	}
 	errs = multierror.Append(errs, s.Engine.Close())
 	return errs.ErrorOrNil()
@@ -1012,4 +1071,14 @@ func (s *DataStore) getSecret(siteID string) (secret string, err error) {
 		return "", fmt.Errorf("site %s disabled", siteID)
 	}
 	return secret, nil
+}
+
+func (s *DataStore) indexCommentForSearch(comment store.Comment) error {
+	if s.SearchService != nil {
+		err := s.SearchService.Index(comment)
+		if err != nil {
+			return fmt.Errorf("can't index comment %s: %w", comment.ID, err)
+		}
+	}
+	return nil
 }