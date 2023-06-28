f3927fca120955843a24ba632d3bc2d898032fd9
Dmitry Verkhoturov <paskal.07@gmail.com>
2023-06-28 22:43:17 +0200

    switch from telegram_bot_username to telegram_notifications
    Bot username is returned as an answer to subscribe request,
    so knowing it in advance is unnecessary.

--- frontend/apps/remark42/app/__stubs__/static-config.ts
+++ frontend/apps/remark42/app/__stubs__/static-config.ts
@@ -16,7 +16,7 @@ beforeEach(() => {
     simple_view: false,
     anon_vote: false,
     email_notifications: false,
-    telegram_bot_username: '',
+    telegram_notifications: false,
     emoji_enabled: true,
   };
 });
